a5ef17288df026584ee921b664f6ff6ed22912a0
Vadim Markovtsev <vadim@athenian.co>
2019-12-05 19:32:24 +0100

    Run black in generate-server.sh
    Signed-off-by: Vadim Markovtsev <vadim@athenian.co>

--- generate-server.sh
+++ generate-server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/spec/openapi.yaml -g python-aiohttp -o /local/server --generate-alias-as-model --git-repo-id athenianco/athenian-api --package-name athenian.api
+black server
