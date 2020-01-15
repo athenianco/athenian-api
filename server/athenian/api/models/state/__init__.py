b2f6eeff25b3dd84522dbbe9572390c1c1757f39
Vadim Markovtsev <vadim@athenian.co>
2020-01-15 11:35:42 +0100

    Check the state DB schema version at startup
    Signed-off-by: Vadim Markovtsev <vadim@athenian.co>

---
+++ server/athenian/api/models/state/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from alembic import script
+from alembic.migration import MigrationContext
+from sqlalchemy import create_engine
+
+from athenian.api import slogging
+
+
+slogging.trailing_dot_exceptions.add("alembic.runtime.migration")
+
+
+class StateDBSchemaVersionMismatchError(Exception):
+    """Error raised if the DB schema versions do not match."""
+
+
+def check_schema_version(conn_str: str) -> None:
+    """Raise StateDBSchemaVersionMismatchError if the real (connected) DB schema version \
+    does not match the required (declared in the code) version."""
+    directory = script.ScriptDirectory(os.path.dirname(__file__))
+    engine = create_engine(conn_str)
+    with engine.begin() as conn:
+        context = MigrationContext.configure(conn)
+        real_rev = context.get_current_revision()
+    req_rev = directory.get_current_head()
+    if real_rev != req_rev:
+        raise StateDBSchemaVersionMismatchError("Required: %s Connected: %s" % (req_rev, real_rev))
