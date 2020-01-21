1b9d4e0d909d3ddef7f5c5336fb6fdef829b880c
Vadim Markovtsev <vadim@athenian.co>
2020-01-21 18:38:39 +0100

    Fix migrations

--- server/athenian/api/models/state/versions/e7afe365ab0e_create_repository_sets_table.py
+++ server/athenian/api/models/state/versions/e7afe365ab0e_create_repository_sets_table.py
@@ -18,9 +18,15 @@ depends_on = None
 
 def upgrade():
     op.create_table(
+        "accounts",
+        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("created_at", sa.TIMESTAMP(), nullable=False),
+    )
+    op.create_table(
         "repository_sets",
         sa.Column("id", sa.Integer(), primary_key=True),
-        sa.Column("owner", sa.String(256), nullable=False),
+        sa.Column("owner", sa.Integer(), sa.ForeignKey("accounts.id", name="fk_reposet_owner"),
+                  nullable=False),
         sa.Column("updated_at", sa.TIMESTAMP(), nullable=False),
         sa.Column("created_at", sa.TIMESTAMP(), nullable=False),
         sa.Column("updates_count", sa.Integer(), nullable=False),
@@ -32,3 +38,4 @@ def upgrade():
 
 def downgrade():
     op.drop_table("repository_sets")
+    op.drop_table("accounts")
