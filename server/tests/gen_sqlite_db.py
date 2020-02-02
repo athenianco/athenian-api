aa4783fc54de538649651d0bccd26816bead831e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-02-03 00:24:13 +0100

    Add the ability to skip inserting fixture date in the state DB in gen_sqlite_db
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/tests/gen_sqlite_db.py
+++ server/tests/gen_sqlite_db.py
@@ -1,3 +1,5 @@
+import argparse
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,20 +20,31 @@ from tests.controllers.conftest import db_dir, metadata_db
 from tests.sample_db_data import fill_state_session
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate DB fixtures.")
+    parser.add_argument("--no-state-samples", action="store_true",
+                        help="Leave the initialized state DB empty.")
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
     metadata_db()
     state_db_path = db_dir / "sdb.sqlite"
     if state_db_path.exists():
         state_db_path.unlink()
+
     conn_str = "sqlite:///%s" % state_db_path
     migrate_state(conn_str, exec=False)
-    engine = create_engine(conn_str)
-    session = sessionmaker(bind=engine)()
-    try:
-        fill_state_session(session)
-        session.commit()
-    finally:
-        session.close()
+
+    if not args.no_state_samples:
+        engine = create_engine(conn_str)
+        session = sessionmaker(bind=engine)()
+        try:
+            fill_state_session(session)
+            session.commit()
+        finally:
+            session.close()
 
 
 if __name__ == "__main__":
