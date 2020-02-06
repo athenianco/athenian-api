8d8ef6867ec21f54c30c845b374f796c3823bac5
Vadim Markovtsev <vadim@athenian.co>
2020-02-06 15:18:55 +0100

    Fix sqlite account id out of range

---
+++ server/tests/test_invite_admin.py
@@ -0,0 +1,11 @@
+import databases
+
+from athenian.api.controllers import invitation_controller
+from athenian.api.invite_admin import main
+
+
+async def test_reset_sequence(state_db):
+    main(state_db)
+    db = databases.Database(state_db)
+    await db.connect()
+    assert await invitation_controller._create_new_account(db) == 4
