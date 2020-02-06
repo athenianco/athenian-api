49b76d04333bd9a92a31f35be098b4497ac5e418
Vadim Markovtsev <vadim@athenian.co>
2020-02-06 09:48:49 +0100

    Print the current state DB schema version in the logs

--- server/athenian/api/models/state/__init__.py
+++ server/athenian/api/models/state/__init__.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from alembic import script
@@ -14,7 +15,7 @@ class StateDBSchemaVersionMismatchError(Exception):
     """Error raised if the DB schema versions do not match."""
 
 
-def check_schema_version(conn_str: str) -> None:
+def check_schema_version(conn_str: str, log: logging.Logger) -> None:
     """Raise StateDBSchemaVersionMismatchError if the real (connected) DB schema version \
     does not match the required (declared in the code) version."""
     directory = script.ScriptDirectory(os.path.dirname(__file__))
@@ -25,3 +26,4 @@ def check_schema_version(conn_str: str) -> None:
     req_rev = directory.get_current_head()
     if real_rev != req_rev:
         raise StateDBSchemaVersionMismatchError("Required: %s Connected: %s" % (req_rev, real_rev))
+    log.info("State DB schema version: %s", real_rev)
