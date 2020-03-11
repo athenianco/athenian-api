9d5186b7d865bf104a0d2c8557b1e0a18734a30a
Vadim Markovtsev <vadim@athenian.co>
2020-03-11 13:07:33 +0100

    Tolerate errors in the metadata schema validator script

--- server/athenian/api/models/metadata/github/__main__.py
+++ server/athenian/api/models/metadata/github/__main__.py
@@ -10,11 +10,11 @@ from athenian.api.models.metadata.github import Base
 def main():
     """Try to fetch one example of each model in the metadata schema from a real DB instance."""
     engine = create_engine(sys.argv[1])
-    session = sessionmaker(bind=engine)()
     print("Checking the metadata schema...", flush=True)
     errors = []
-    try:
-        for model in Base._decl_class_registry.values():
+    for model in Base._decl_class_registry.values():
+        session = sessionmaker(bind=engine)()
+        try:
             try:
                 model.__name__, model.__tablename__
             except AttributeError:
@@ -27,13 +27,13 @@ def main():
             else:
                 status = "✔️"
             print("%s  %s / %s" % (status, model.__name__, model.__tablename__), flush=True)
-    finally:
-        session.close()
-        for model, exc in errors:
-            print("=" * 80, file=sys.stderr)
-            print("%s / %s\n" % (model.__name__, model.__tablename__), file=sys.stderr)
-            print(exc, file=sys.stderr)
-            print(file=sys.stderr)
+        finally:
+            session.close()
+    for model, exc in errors:
+        print("=" * 80, file=sys.stderr)
+        print("%s / %s\n" % (model.__name__, model.__tablename__), file=sys.stderr)
+        print(exc, file=sys.stderr)
+        print(file=sys.stderr)
 
 
 if __name__ == "__main__":
