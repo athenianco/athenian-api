d767c67862146342f5e2526d6115a380f679548b
Vadim Markovtsev <vadim@athenian.co>
2020-04-13 09:05:05 +0200

    Fix wrong authors for branch-matched releases ENG-563

--- server/athenian/api/controllers/miners/github/release.py
+++ server/athenian/api/controllers/miners/github/release.py
@@ -161,8 +161,12 @@ async def _match_releases_by_branch(repos: Iterable[str],
                 PushCommit.committed_date.between(time_from, time_to)))
             .order_by(desc(PushCommit.commit_date)),
             conn, PushCommit)
+        authors = commits[PushCommit.author_login.key]
+        not_gh_merge = ((commits[PushCommit.committer_name.key] != "GitHub")
+                        | (commits[PushCommit.committer_email.key] != "noreply@github.com"))
+        authors[not_gh_merge] = commits[PushCommit.committer_login.key][not_gh_merge]
         pseudo_releases.append(pd.DataFrame({
-            Release.author.key: commits[PushCommit.author_login.key],
+            Release.author.key: authors,
             Release.commit_id.key: commits[PushCommit.node_id.key],
             Release.id.key: commits[PushCommit.node_id.key] + "_" + repo,
             Release.name.key: commits[PushCommit.sha.key],
