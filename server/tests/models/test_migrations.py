61cff7cb0f5a4353680650997ed0fe7947c80e6a
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-05-11 22:35:34 +0200

    Make test_migration run fine locally for OSX as well
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/tests/models/test_migrations.py
+++ server/tests/models/test_migrations.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 import tempfile
@@ -11,8 +12,13 @@ from athenian.api.models.state.models import Base
 def test_migrations():
     with tempfile.TemporaryDirectory() as tmpdir:
         cs = "sqlite:///%s/sdb.sqlite" % tmpdir
-        subprocess.run([sys.executable, "-m", "athenian.api.models.state", cs],
-                       check=True, cwd=tmpdir)
+        env = {**os.environ, "PYTHONPATH": os.getcwd()}
+        try:
+            subprocess.run([sys.executable, "-m", "athenian.api.models.state", cs],
+                           capture_output=True, check=True, cwd=tmpdir, env=env)
+        except subprocess.CalledProcessError as err:
+            raise RuntimeError(err.stderr.decode("utf8")) from err
+
         engine = create_engine(cs)
         session = sessionmaker(bind=engine)()
         try:
