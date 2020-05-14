47bd028e2fe48a52293fe540405069cbecf4fd61
Vadim Markovtsev <vadim@athenian.co>
2020-05-14 13:45:29 +0200

    Add precomputed DB index (thx @dennwc)

---
+++ server/athenian/api/models/precomputed/versions/49c42f5d53b5_index_github_pull_request_times.py
@@ -0,0 +1,35 @@
+"""Index github_pull_request_times
+
+Revision ID: 49c42f5d53b5
+Revises: 2c6ec8d26e72
+Create Date: 2020-05-14 11:41:39.839151+00:00
+
+"""
+from alembic import op
+from sqlalchemy.orm import Session
+
+
+# revision identifiers, used by Alembic.
+revision = "49c42f5d53b5"
+down_revision = "2c6ec8d26e72"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    sql = """
+    CREATE INDEX github_pull_request_times_main
+    ON github_pull_request_times
+    ("repository_full_name", "pr_created_at", "pr_done_at");
+    """
+    bind = op.get_bind()
+    if bind.dialect.name == "postgresql":
+        session = Session(bind=bind)
+        session.execute(sql)
+
+
+def downgrade():
+    bind = op.get_bind()
+    if bind.dialect.name == "postgresql":
+        session = Session(bind=bind)
+        session.execute("DROP INDEX github_pull_request_times_main;")
