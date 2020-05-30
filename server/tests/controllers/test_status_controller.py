562c33f6bd13b06aec1cde287b1f9453ca062ed2
Vadim Markovtsev <vadim@athenian.co>
2020-05-30 21:56:44 +0200

    Cache /status

--- server/tests/controllers/test_status_controller.py
+++ server/tests/controllers/test_status_controller.py
@@ -12,10 +12,12 @@ async def test_versions(client, headers):
     assert body["api"] == metadata.__version__
 
 
-async def test_status(client, headers):
-    response = await client.request(
-        method="GET", path="/status", headers=headers, json={},
-    )
-    assert response.status == 200
-    body = (await response.read()).decode("utf-8")
-    assert "requests_total" in body
+async def test_status(client, headers, client_cache, app):
+    app._cache = client_cache
+    for _ in range(2):
+        response = await client.request(
+            method="GET", path="/status", headers=headers, json={},
+        )
+        assert response.status == 200
+        body = (await response.read()).decode("utf-8")
+        assert "requests_total" in body
