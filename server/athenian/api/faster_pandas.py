345009182ecd2d6b76ea9b3862566b58938d2a18
Vadim Markovtsev <vadim@athenian.co>
2020-06-11 12:03:20 +0200

    Optimize PR iteration by 7%

--- server/athenian/api/faster_pandas.py
+++ server/athenian/api/faster_pandas.py
@@ -1,5 +1,8 @@
 from datetime import timezone
+from functools import lru_cache
 
+from pandas.core import algorithms
+from pandas.core.arrays import datetimes
 from pandas.core.arrays.datetimelike import DatetimeLikeArrayMixin
 from pandas.core.dtypes.dtypes import DatetimeTZDtype
 from pandas.core.internals import Block
@@ -26,18 +29,27 @@ def patch_pandas():
         return original_take(self, indices, allow_fill=False)
 
     DatetimeLikeArrayMixin.take = fast_take
-    ftype_cache = {}
     original_ftype_getter = Block.ftype.getter
+    ftype_cache = {}
 
     def fast_ftype(self):
-        if getattr(self.values, "_pandas_ftype", False):
-            dtype = self.dtype.subtype
-        else:
-            dtype = self.dtype
+        key = (self.dtype, self._ftype)
         try:
-            return ftype_cache[(dtype, self._ftype)]
+            return ftype_cache[key]
         except KeyError:
-            ftype_cache[(dtype, self._ftype)] = r = original_ftype_getter(self)
-            return r
+            ftype_cache[key] = ftype = original_ftype_getter(self)
+            return ftype
 
     Block.ftype = property(fast_ftype)
+
+    original_get_take_nd_function = algorithms._get_take_nd_function
+    cached_get_take_nd_function = lru_cache(algorithms._get_take_nd_function)
+
+    def _get_take_nd_function(ndim: int, arr_dtype, out_dtype, axis: int = 0, mask_info=None):
+        if mask_info is None or not mask_info[1]:
+            return cached_get_take_nd_function(ndim, arr_dtype, out_dtype, axis)
+        return original_get_take_nd_function(ndim, arr_dtype, out_dtype, axis, mask_info)
+
+    algorithms._get_take_nd_function = _get_take_nd_function
+
+    datetimes._validate_dt64_dtype = lru_cache(datetimes._validate_dt64_dtype)
