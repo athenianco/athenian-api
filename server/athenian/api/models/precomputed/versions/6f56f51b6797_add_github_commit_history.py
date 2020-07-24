a212e0fc724a81f139ec2d75dab5266a3e6444f9
Vadim Markovtsev <vadim@athenian.co>
2020-07-24 14:17:24 +0200

    Set the user name length limit to 39 in the pdb

--- server/athenian/api/models/precomputed/versions/6f56f51b6797_add_github_commit_history.py
+++ server/athenian/api/models/precomputed/versions/6f56f51b6797_add_github_commit_history.py
@@ -19,7 +19,7 @@ depends_on = None
 def upgrade():
     op.create_table(
         "github_commit_history",
-        sa.Column("repository_full_name", sa.String(64 + 1 + 100), primary_key=True),
+        sa.Column("repository_full_name", sa.String(39 + 1 + 100), primary_key=True),
         sa.Column("format_version", sa.Integer(), primary_key=True, server_default="1"),
         sa.Column("dag", sa.LargeBinary(), nullable=False),
         sa.Column("updated_at", sa.TIMESTAMP(timezone=True), nullable=False,
--- server/athenian/api/models/precomputed/versions/87f211ffe876_add_github_merged_commits.py
+++ server/athenian/api/models/precomputed/versions/87f211ffe876_add_github_merged_commits.py
@@ -28,7 +28,7 @@ def upgrade():
         "github_merged_commits",
         sa.Column("pr_node_id", sa.CHAR(32), primary_key=True),
         sa.Column("release_match", sa.Text(), primary_key=True),
-        sa.Column("repository_full_name", sa.String(64 + 1 + 100), nullable=False),
+        sa.Column("repository_full_name", sa.String(39 + 1 + 100), nullable=False),
         sa.Column("checked_releases", hs, nullable=False, server_default=""),
         sa.Column("updated_at", sa.TIMESTAMP(timezone=True), nullable=False,
                   server_default=sa.func.now()),
