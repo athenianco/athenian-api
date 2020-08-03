c441ae742979954204020d78155bccc6e7e78780
Vadim Markovtsev <vadim@athenian.co>
2020-08-03 19:09:19 +0200

    [DEV-613] Query by user IDs in /filter/commits

---
+++ server/tests/controllers/miners/github/test_commit.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+
+from athenian.api.controllers.miners.github.commit import extract_commits, FilterCommitsProperty
+
+
+async def test_extract_commits_users(mdb):
+    commits = await extract_commits(FilterCommitsProperty.NO_PR_MERGES,
+                                    datetime(2015, 1, 1, tzinfo=timezone.utc),
+                                    datetime(2020, 6, 1, tzinfo=timezone.utc),
+                                    ["src-d/go-git"],
+                                    ["mcuadros"],
+                                    ["mcuadros"],
+                                    mdb, None)
+    assert len(commits) == 750
