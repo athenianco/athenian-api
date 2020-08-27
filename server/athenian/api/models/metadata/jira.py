6840694cff8e4234dc6b55f3d6ff58e5e54a17ca
Vadim Markovtsev <vadim@athenian.co>
2020-08-27 19:53:44 +0200

    [DEV-772] Add JIRA schema

---
+++ server/athenian/api/models/metadata/jira.py
@@ -0,0 +1,32 @@
+from sqlalchemy import ARRAY, BigInteger, Boolean, Column, JSON, Text, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Epic(Base):
+    __tablename__ = "jira.epic"
+
+    acc_id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
+    key = Column(Text, nullable=False)
+    name = Column(Text, nullable=False)
+    title = Column(Text, nullable=False)
+    done = Column(Boolean, nullable=False)
+
+
+class Issue(Base):
+    __tablename__ = "jira.issue"
+
+    acc_id = Column(BigInteger, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
+    key = Column(Text, nullable=False)
+    title = Column(Text, nullable=False)
+    type = Column(Text, nullable=False)
+    status = Column(Text)
+    labels = ARRAY(Text).with_variant(JSON, "sqlite")
+    epic_id = Column(BigInteger)
+    created = Column(TIMESTAMP(timezone=True), nullable=False)
+    updated = Column(TIMESTAMP(timezone=True), nullable=False)
+    resolved = Column(TIMESTAMP(timezone=True))
