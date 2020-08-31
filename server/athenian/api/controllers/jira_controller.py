5790218d88cbe930b3c4cd176a758bf782a0a6cd
Vadim Markovtsev <vadim@athenian.co>
2020-08-31 14:31:27 +0200

    [DEV-787] Generate the models for /filter/jira

--- server/athenian/api/controllers/jira_controller.py
+++ server/athenian/api/controllers/jira_controller.py
@@ -1,8 +1,17 @@
 from aiohttp import web
 
+from athenian.api.models.web import InvalidRequestError
+from athenian.api.models.web.filter_jira_stuff import FilterJIRAStuff
 from athenian.api.request import AthenianWebRequest
+from athenian.api.response import ResponseError
 
 
 async def filter_jira_stuff(request: AthenianWebRequest, body: dict) -> web.Response:
     """Find JIRA epics and labels used in the specified date interval."""
+    try:
+        filt = FilterJIRAStuff.from_dict(body)
+    except ValueError as e:
+        # for example, passing a date with day=32
+        return ResponseError(InvalidRequestError("?", detail=str(e))).response
+    _ = filt
     raise NotImplementedError
