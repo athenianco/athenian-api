4e8e1a92749ccb32995fce3c2420388e076d0247
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2020-10-12 09:57:20 +0200

    Lint files
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/athenian/api/experiments/indexing/indexer.py
+++ server/athenian/api/experiments/indexing/indexer.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 
 class LayerLevel(Enum):
+    """Represent a the granularity level of a layer in the indexer."""
+
     MINUTE = 0
     HOUR = 1
     DAY = 2
@@ -18,44 +20,55 @@ class LayerLevel(Enum):
 
     @classmethod
     def levels(cls):
+        """Return the levels mapping by values."""
         return {v.value: v for k, v in cls.__members__.items()}
 
     @classmethod
     def min(cls):
+        """Return the minimum level."""
         levels = cls.levels()
         return levels[min(levels)]
 
     @classmethod
     def max(cls):
+        """Return the maximum level."""
         levels = cls.levels()
         return levels[max(levels)]
 
     def get_deeper_level(self):
+        """Return the next lower level."""
         return self.levels()[self.value - 1]
 
     def get_shallower_level(self):
+        """Return the next higher level."""
         return self.levels()[self.value + 1]
 
 
 class BaseIndexer:
+    """Base logic of an indexer."""
 
     __instance = None
     __slots__ = ["_layers", "_virtual_indexes", "_last_update"]
 
-    def __init__(self,
-                 layers: Dict[LayerLevel, np.ndarray],
-                 virtual_indexes: Dict[LayerLevel, np.ndarray],
-                 last_update: datetime):
+    def __init__(
+        self,
+        layers: Dict[LayerLevel, np.ndarray],
+        virtual_indexes: Dict[LayerLevel, np.ndarray],
+        last_update: datetime,
+    ):
+        """Initialize an indexer with the provided input."""
         self._layers = layers
         self._virtual_indexes = virtual_indexes
         self._last_update = last_update
 
     @property
     def last_update(self):
+        """Return the last update of the indexer."""
         return self._last_update
 
     @classmethod
     async def get_instance(cls, mdb_conn):
+        """Return or create the singleton indexer instance."""
         if cls.__instance is not None:
             return cls.__instance
 
@@ -63,28 +76,36 @@ class BaseIndexer:
 
     @classmethod
     async def create(cls, mdb_conn):
+        """Create an indexer."""
         return cls(*(await cls._get_init_params(mdb_conn)))
 
     async def full_refresh(self, mdb_conn):
+        """Update the indexer by resetting the data and fully refreshing the layers."""
         layers, virtual_indexes, last_update = await self._get_init_params(mdb_conn)
         self._layers = layers
         self._virtual_indexes = virtual_indexes
         self._last_update = last_update
 
     async def refresh(self, mdb_conn):
+        """Update the indexer by adding new data."""
         # same query, but created_at >= last_update
         raise NotImplementedError()
 
     def search(self, date_from, date_to, return_counts=False):
+        """Seach for the data in the provided interval."""
         vi_index_from = self._indexify(date_from)
         vi_index_to = self._indexify(date_to)
 
         v = self._search_in_layer(LayerLevel.max(), vi_index_from, vi_index_to)
-        return np.unique(v[v != None], return_counts=return_counts)
+        return np.unique(v[np.nonzero(v)], return_counts=return_counts)
 
     def _search_in_layer(self, layer_level, vi_index_from, vi_index_to):
-        index_from = bisect.bisect_left(self._virtual_indexes[layer_level], vi_index_from)
-        index_to = bisect.bisect_left(self._virtual_indexes[layer_level], vi_index_to) - 1
+        index_from = bisect.bisect_left(
+            self._virtual_indexes[layer_level], vi_index_from,
+        )
+        index_to = (
+            bisect.bisect_left(self._virtual_indexes[layer_level], vi_index_to) - 1
+        )
 
         if layer_level.value == 0:
             index_to += 1
@@ -93,27 +114,34 @@ class BaseIndexer:
 
             postings_lists = self._layers[layer_level][index_from:index_to]
             v = np.concatenate(postings_lists)
-            print(f"Found {len(postings_lists)} to concatenate in layer {layer_level.name} "
-                  f"(total items: {len(v)})")
+            print(
+                f"Found {len(postings_lists)} to concatenate in layer {layer_level.name} "
+                f"(total items: {len(v)})",
+            )
             return v
 
         if index_from >= index_to:
-            return self._search_in_layer(layer_level.get_deeper_level(),
-                                         vi_index_from, vi_index_to)
+            return self._search_in_layer(
+                layer_level.get_deeper_level(), vi_index_from, vi_index_to,
+            )
 
         left = self._search_in_layer(
             layer_level.get_deeper_level(),
-            vi_index_from, self._virtual_indexes[layer_level][index_from],
+            vi_index_from,
+            self._virtual_indexes[layer_level][index_from],
         )
         right = self._search_in_layer(
             layer_level.get_deeper_level(),
-            self._virtual_indexes[layer_level][index_to], vi_index_to,
+            self._virtual_indexes[layer_level][index_to],
+            vi_index_to,
         )
         postings_lists = self._layers[layer_level][index_from:index_to]
         center = np.concatenate(postings_lists)
         v = np.concatenate([left, center, right])
-        print(f"Found {len(postings_lists)} to concatenate in layer {layer_level.name} "
-              f"(total items: {len(center)} | grand total items: {len(v)})")
+        print(
+            f"Found {len(postings_lists)} to concatenate in layer {layer_level.name} "
+            f"(total items: {len(center)} | grand total items: {len(v)})",
+        )
         return v
 
     @classmethod
@@ -138,7 +166,8 @@ class BaseIndexer:
         df["timestamp_d"] = df["timestamp_h"].apply(lambda t: t.replace(hour=0))
         df["timestamp_mo"] = df["timestamp_d"].apply(lambda t: t.replace(day=1))
         df["timestamp_q"] = df["timestamp_mo"].apply(
-            lambda t: t.replace(month={0: 1, 1: 4, 2: 7, 3: 10}[(t.month - 1) // 3]))
+            lambda t: t.replace(month={0: 1, 1: 4, 2: 7, 3: 10}[(t.month - 1) // 3]),
+        )
         print("Dataframe prepared!")
 
         layer_level = LayerLevel.MINUTE
@@ -150,10 +179,12 @@ class BaseIndexer:
         v_indexes = {LayerLevel.MINUTE: virtual_indexes(df_m)}
         print(f"{layer_level} built")
 
-        for layer_level, col in [(LayerLevel.HOUR, "timestamp_h"),
-                                 (LayerLevel.DAY, "timestamp_d"),
-                                 (LayerLevel.MONTH, "timestamp_mo"),
-                                 (LayerLevel.QUARTER, "timestamp_q")]:
+        for layer_level, col in [
+            (LayerLevel.HOUR, "timestamp_h"),
+            (LayerLevel.DAY, "timestamp_d"),
+            (LayerLevel.MONTH, "timestamp_mo"),
+            (LayerLevel.QUARTER, "timestamp_q"),
+        ]:
             print(f"Building for layer {layer_level}...")
             df_level = df.groupby(col).agg({"values": sum}).reset_index()
             df_level["values"] = df_level["values"].apply(lambda l: list(set(l)))
@@ -192,6 +223,7 @@ class BaseIndexer:
 
 
 class AuthorIndexer(BaseIndexer):
+    """Indexer for PRs' authors."""
 
     sql_query = """
 SELECT
@@ -204,10 +236,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class ReviewerIndexer(BaseIndexer):
+    """Indexer for PRs' reviewers."""
 
     sql_query = """
 SELECT
@@ -220,10 +253,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class CommitAuthorIndexer(BaseIndexer):
+    """Indexer for PRs' commits authors."""
 
     sql_query = """
 SELECT
@@ -236,10 +270,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class CommitCommitterIndexer(BaseIndexer):
+    """Indexer for PRs' commits committers."""
 
     sql_query = """
 SELECT
@@ -252,10 +287,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class CommenterIndexer(BaseIndexer):
+    """Indexer for PRs' commenters."""
 
     sql_query = """
 SELECT
@@ -268,10 +304,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class MergerIndexer(BaseIndexer):
+    """Indexer for PRs' mergers."""
 
     sql_query = """
 SELECT
@@ -285,10 +322,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class ReleaserIndexer(BaseIndexer):
+    """Indexer for PRs' releasers."""
 
     sql_query = """
 SELECT
@@ -301,10 +339,11 @@ FROM (
 ) q
 GROUP BY timestamp
 ORDER BY timestamp;
-    """
+    """  # noqa: E501
 
 
 class ContributorsIndexer:
+    """Indexer for PRs' contributors as a composite of other indexers."""
 
     indexers = [
         AuthorIndexer,
@@ -317,15 +356,18 @@ class ContributorsIndexer:
     ]
 
     def __init__(self, indexers):
+        """Initialize a contribs indexer with the provided sub-indexers."""
         self._indexers = indexers
 
     @classmethod
     async def create(cls, mdb_conn):
+        """Create a contributors indexer."""
         tasks = [ind_cls.get_instance(mdb_conn) for ind_cls in cls.indexers]
         indexers = await asyncio.gather(*tasks, return_exceptions=True)
         return cls(indexers)
 
     def search(self, date_from, date_to, op, return_counts=False):
+        """Search for contriburos in the provided interval and `op` logic."""
         if op.strip().upper() == "AND":
             func = np.intersect1d
         elif op.strip().upper() == "OR":
@@ -334,5 +376,10 @@ class ContributorsIndexer:
             raise ValueError(f"Unsupported operator `{op}`")
 
         # handle return counts = true
-        return reduce(func, [ind.search(date_from, date_to, return_counts=return_counts)
-                             for ind in self._indexers])
+        return reduce(
+            func,
+            [
+                ind.search(date_from, date_to, return_counts=return_counts)
+                for ind in self._indexers
+            ],
+        )
