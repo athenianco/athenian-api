31d886206f84699d55ba86447879da024b5e7d95
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-04-20 17:19:39 +0200

    Make get_calculator_for_user accept multiple services
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/athenian/api/controllers/calculator_selector.py
+++ server/athenian/api/controllers/calculator_selector.py
@@ -1,8 +1,9 @@
-from typing import Callable, Optional, Tuple
+from typing import Callable, List, Optional, Tuple, Union
 
 import aiomcache
 from sqlalchemy import and_, select
 
+from athenian.api.async_utils import gather
 from athenian.api.controllers.features.entries import get_calculator
 from athenian.api.models.state.models import AccountFeature, Feature, \
     FeatureComponent
@@ -10,7 +11,7 @@ from athenian.api.typing_utils import DatabaseLike
 
 
 async def get_calculator_for_user(
-    service: str,
+    service: Union[str, List[str]],
     account_id: int,
     meta_ids: Tuple[int, ...],
     user_id: str,
@@ -23,14 +24,41 @@ async def get_calculator_for_user(
     base_module: Optional[str] = "athenian.api.experiments",
 ) -> Callable:
     """Get the metrics calculator function for the given user."""
+    async def _get_calculator_for_service(s):
+        return await _get_calculator_for_user(
+            s, account_id, meta_ids, user_id, god_id,
+            sdb, mdb, pdb, rdb, cache, base_module=base_module)
 
+    if isinstance(service, str):
+        return await _get_calculator_for_service(service)
+    else:
+        tasks = await gather(*[_get_calculator_for_service(s) for s in service])
+        return dict(zip(service, tasks))
+
+
+async def _get_calculator_for_user(
+    service: str,
+    account_id: int,
+    meta_ids: Tuple[int, ...],
+    user_id: str,
+    god_id: Optional[str],
+    sdb: DatabaseLike,
+    mdb: DatabaseLike,
+    pdb: DatabaseLike,
+    rdb: DatabaseLike,
+    cache: Optional[aiomcache.Client],
+    base_module: Optional[str] = "athenian.api.experiments",
+) -> Callable:
     def _get_calculator(variation=None):
         return get_calculator(
             service, account_id, meta_ids, mdb, pdb, rdb, cache,
             variation=variation, base_module=base_module,
         )
 
-    feature_name_prefix = METRIC_ENTRIES_VARIATIONS_PREFIX[service]
+    feature_name_prefix = METRIC_ENTRIES_VARIATIONS_PREFIX.get(service)
+    if not feature_name_prefix:
+        return _get_calculator()
+
     all_metrics_variations_features = await sdb.fetch_all(
         select([Feature.id, Feature.name, Feature.default_parameters]).where(
             and_(
--- server/tests/controllers/test_calculator_selector.py
+++ server/tests/controllers/test_calculator_selector.py
@@ -121,6 +121,37 @@ async def test_get_calculator_for_user_with_feature(
     assert isinstance(calc, MetricEntriesCalculator)
 
 
+async def test_get_calculator_for_user_with_feature_multiple_services(
+        sdb, mdb, pdb, rdb, cache, base_testing_module, current_module,
+):
+    feature_id = await sdb.execute(
+        insert(Feature).values(
+            Feature(
+                name=f"{METRIC_ENTRIES_VARIATIONS_PREFIX['github']}test_calculator_selector",
+                component=FeatureComponent.server,
+                enabled=True,
+            )
+            .create_defaults()
+            .explode(),
+        ),
+    )
+    await sdb.execute(
+        insert(AccountFeature).values(
+            AccountFeature(account_id=1, feature_id=feature_id, enabled=True)
+            .create_defaults()
+            .explode(with_primary_keys=True),
+        ),
+    )
+
+    calcs = await get_calculator_for_user(
+        ["github", "bitbucket"], 1, (1, ), "1", None, sdb, mdb, pdb, rdb, cache,
+        base_module=base_testing_module,
+    )
+    assert len(calcs) == 2
+    assert isinstance(calcs["github"], MetricEntriesCalculator)
+    assert isinstance(calcs["bitbucket"], OriginalMetricEntriesCalculator)
+
+
 @pytest.mark.parametrize("is_god", (True, False))
 async def test_get_calculator_for_user_with_feature_god_only(
         sdb, mdb, pdb, rdb, cache, is_god, base_testing_module, current_module,
