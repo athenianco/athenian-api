16891cc0a081f9418626e03cd247da132e73f768
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-05-13 10:59:32 +0200

    Prepare scaffolding for preloading parts of PreloadedOpenPRFactsLoader
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/athenian/api/experiments/preloading/entries.py
+++ server/athenian/api/experiments/preloading/entries.py
@@ -13,6 +13,7 @@ from athenian.api.controllers.features.github.unfresh_pull_request_metrics impor
     UnfreshPullRequestFactsFetcher
 from athenian.api.controllers.miners.filters import JIRAFilter, LabelFilter
 from athenian.api.controllers.miners.github.branches import BranchMiner
+from athenian.api.controllers.miners.github.precomputed_prs import OpenPRFactsLoader
 from athenian.api.controllers.miners.github.pull_request import PullRequestMiner
 from athenian.api.controllers.miners.github.release_load import \
     match_groups_to_conditions, ReleaseLoader
@@ -106,10 +107,17 @@ class PreloadedReleaseToPullRequestMapper(ReleaseToPullRequestMapper):
     release_loader = PreloadedReleaseLoader
 
 
+class PreloadedOpenPRFactsLoader(OpenPRFactsLoader):
+    """Loader for preloaded open PRs facts."""
+
+    pass
+
+
 class PreloadedUnfreshPullRequestFactsFetcher(UnfreshPullRequestFactsFetcher):
     """Fetcher for preloaded unfresh pull requests facts."""
 
     release_loader = PreloadedReleaseLoader
+    open_prs_facts_loader = PreloadedOpenPRFactsLoader
 
 
 class PreloadedBranchMiner(BranchMiner):
