2d1b062a1f1c2f88dd6f98cef0beae82bd3cb4ee
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-05-13 18:23:09 +0200

    Add scaffolding for preloading load_merged_unreleased_pull_request_facts
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- server/athenian/api/experiments/preloading/entries.py
+++ server/athenian/api/experiments/preloading/entries.py
@@ -14,7 +14,8 @@ from athenian.api.controllers.features.github.unfresh_pull_request_metrics impor
     UnfreshPullRequestFactsFetcher
 from athenian.api.controllers.miners.filters import JIRAFilter, LabelFilter
 from athenian.api.controllers.miners.github.branches import BranchMiner
-from athenian.api.controllers.miners.github.precomputed_prs import OpenPRFactsLoader
+from athenian.api.controllers.miners.github.precomputed_prs import \
+    MergedPRFactsLoader, OpenPRFactsLoader
 from athenian.api.controllers.miners.github.pull_request import PullRequestMiner
 from athenian.api.controllers.miners.github.release_load import \
     match_groups_to_conditions, ReleaseLoader
@@ -111,6 +112,12 @@ class PreloadedReleaseToPullRequestMapper(ReleaseToPullRequestMapper):
     release_loader = PreloadedReleaseLoader
 
 
+class PreloadedMergedPRFactsLoader(MergedPRFactsLoader):
+    """Loader for preloaded merged PRs facts."""
+
+    pass
+
+
 class PreloadedOpenPRFactsLoader(OpenPRFactsLoader):
     """Loader for preloaded open PRs facts."""
 
@@ -179,6 +186,7 @@ class PreloadedUnfreshPullRequestFactsFetcher(UnfreshPullRequestFactsFetcher):
 
     release_loader = PreloadedReleaseLoader
     open_prs_facts_loader = PreloadedOpenPRFactsLoader
+    merged_prs_facts_loader = PreloadedMergedPRFactsLoader
 
 
 class PreloadedBranchMiner(BranchMiner):
