1ad2a361bc9e215e6ae2125d8cb86fdcece9cd19
Vadim Markovtsev <vadim@athenian.co>
2021-06-28 13:01:45 +0200

    [DEV-2492] New perdata tables to support deployment notifications

--- server/athenian/api/models/persistentdata/versions/5aa9506f1374_add_deployment_notifications.py
+++ server/athenian/api/models/persistentdata/versions/5aa9506f1374_add_deployment_notifications.py
@@ -20,13 +20,15 @@ def upgrade():
     name = "deployment_notifications"
     if op.get_bind().dialect.name == "postgresql":
         schema_arg = {"schema": "athenian"}
+        schema = "athenian."
     else:
         name = "athenian." + name
         schema_arg = {}
+        schema = ""
     op.create_table(
         name,
         sa.Column("account_id", sa.Integer(), primary_key=True),
-        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("id", sa.BigInteger(), primary_key=True),
 
         sa.Column("conclusion", sa.Text()),
         sa.Column("environment", sa.Text(), nullable=False),
@@ -44,24 +46,30 @@ def upgrade():
     op.create_table(
         name.replace("deployment_notifications", "deployed_labels"),
         sa.Column("account_id", sa.Integer(), primary_key=True),
-        sa.Column("deployment_id", sa.Integer(),
-                  sa.ForeignKey(f"{name}.id", name="fk_deployed_labels_deployment"),
-                  primary_key=True),
-        sa.Column("label", sa.Text(), primary_key=True),
+        sa.Column("deployment_id", sa.BigInteger(), primary_key=True),
+        sa.Column("key", sa.Text(), primary_key=True),
         sa.Column("value", sa.Text()),
+        sa.ForeignKeyConstraint(
+            ("account_id", "deployment_id"),
+            (f"{schema}{name}.account_id", f"{schema}{name}.id"),
+            name="fk_deployed_labels_deployment",
+        ),
         **schema_arg,
     )
     op.create_table(
         name.replace("deployment_notifications", "deployed_components"),
         sa.Column("account_id", sa.Integer(), primary_key=True),
-        sa.Column("deployment_id", sa.Integer(),
-                  sa.ForeignKey(f"{name}.id", name="fk_deployed_components_deployment"),
-                  primary_key=True),
+        sa.Column("deployment_id", sa.BigInteger(), primary_key=True),
         sa.Column("repository_node_id", sa.Text(), primary_key=True),
         sa.Column("reference", sa.Text(), primary_key=True),
         sa.Column("resolved_commit_node_id", sa.Text()),
         sa.Column("created_at", sa.TIMESTAMP(timezone=True), nullable=False,
                   server_default=sa.func.now()),
+        sa.ForeignKeyConstraint(
+            ("account_id", "deployment_id"),
+            (f"{schema}{name}.account_id", f"{schema}{name}.id"),
+            name="fk_deployed_components_deployment",
+        ),
         **schema_arg,
     )
 
