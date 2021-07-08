698dd0cb138936888b1f87b7c0f2df5c5f463ae3
Vadim Markovtsev <vadim@athenian.co>
2021-07-08 13:15:57 +0200

    [DEV-2583] Require one deployment event with all the data

---
+++ server/athenian/api/models/persistentdata/versions/8176e5ae8ff2_disable_separate_deployment_events_.py
@@ -0,0 +1,39 @@
+"""Disable separate deployment events submission
+
+Revision ID: 8176e5ae8ff2
+Revises: 876b48cd3813
+Create Date: 2021-07-08 09:00:37.673031+00:00
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "8176e5ae8ff2"
+down_revision = "876b48cd3813"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    name = "deployment_notifications"
+    if op.get_bind().dialect.name == "postgresql":
+        schema_arg = {"schema": "athenian"}
+    else:
+        name = "athenian." + name
+        schema_arg = {}
+    with op.batch_alter_table(name, **schema_arg) as bop:
+        bop.alter_column("finished_at", nullable=False)
+        bop.alter_column("conclusion", nullable=False)
+
+
+def downgrade():
+    name = "deployment_notifications"
+    if op.get_bind().dialect.name == "postgresql":
+        schema_arg = {"schema": "athenian"}
+    else:
+        name = "athenian." + name
+        schema_arg = {}
+    with op.batch_alter_table(name, **schema_arg) as bop:
+        bop.alter_column("finished_at", nullable=True)
+        bop.alter_column("conclusion", nullable=True)
