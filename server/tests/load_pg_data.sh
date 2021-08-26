2bf4f8100da6033737012138bc8ba9a1f64dcab3
Vadim Markovtsev <vadim@athenian.co>
2021-08-26 18:58:41 +0200

    Make the DB dumps plain text

--- server/tests/load_pg_data.sh
+++ server/tests/load_pg_data.sh
@@ -15,14 +15,14 @@ export DUMPS_DIR=/db_dumps
 
 dump() {
     echo "Dumping database $1..."
-    pg_dump --host ${POSTGRES_SOURCE_HOST} --port=${POSTGRES_SOURCE_PORT} --no-owner --username=${POSTGRES_SOURCE_USER} -Fc --dbname=$1 | grep -v -E 'CREATE EXTENSION.*pg_repack' > ${DUMPS_DIR}/$1.dump
+    pg_dump --host ${POSTGRES_SOURCE_HOST} --port=${POSTGRES_SOURCE_PORT} --no-owner --username=${POSTGRES_SOURCE_USER} --dbname=$1 | grep -v -E 'CREATE (EXTENSION|COMMENT).*pg_repack' > ${DUMPS_DIR}/$1.dump
     echo "Dump done."
 }
 
 restore() {
     echo "Restoring database $1..."
     createdb -U ${POSTGRES_USER} --lc-collate='C.UTF-8' -T template0 $1
-    pg_restore -x -Fc -O -U ${POSTGRES_USER} -d $1 ${DUMPS_DIR}/$1.dump
+    pg_restore -x -O -U ${POSTGRES_USER} -d $1 ${DUMPS_DIR}/$1.dump
     echo "Restore done."
 }
 
