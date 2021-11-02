0346f198b9e98f0a7cdf914c6ea18f6717a0a725
Vadim Markovtsev <vadim@athenian.co>
2021-11-02 13:10:21 +0100

    [DEV-3000] Implement CodeCheckRunStatistics.critical and stddev

--- server/tests/controllers/test_integrations_controller.py
+++ server/tests/controllers/test_integrations_controller.py
@@ -149,7 +149,7 @@ async def test_get_everything_smoke(client, headers, dummy_deployment_label):
         "check_run_node_id", "check_suite_conclusion", "check_suite_node_id", "check_suite_status",
         "commit_node_id", "committed_date", "completed_at", "conclusion", "deletions", "name",
         "pull_request_node_id", "repository_full_name", "repository_node_id", "sha", "started_at",
-        "status", "url", "check_suite_started", "pull_request_created_at",
+        "status", "url", "check_suite_started", "check_suite_completed", "pull_request_created_at",
         "pull_request_closed_at", "pull_request_merged",
     }
     assert len(jira_issues_df) == 1797
