ae850628b6dac8a666216cd36f648ba74dd5ed1e
Vadim Markovtsev <vadim@athenian.co>
2021-11-17 14:25:24 +0100

    [DEV-3210] Add "api_channel" product feature

---
+++ server/athenian/api/models/state/versions/eb0de1efbe52_api_channel_feature.py
@@ -0,0 +1,30 @@
+"""API channel feature
+
+Revision ID: eb0de1efbe52
+Revises: de3819e9b84d
+Create Date: 2021-11-17 13:16:59.991522+00:00
+
+"""
+import sqlite3
+
+from alembic import op
+import psycopg2
+
+
+# revision identifiers, used by Alembic.
+revision = "eb0de1efbe52"
+down_revision = "de3819e9b84d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    try:
+        op.execute("insert into features(name, component, enabled, default_parameters) "
+                   "values ('api_channel', 'webapp', true, '\"stable\"');")
+    except (sqlite3.IntegrityError, psycopg2.IntegrityError):
+        pass
+
+
+def downgrade():
+    op.execute("delete from features where name = 'api_channel' and component = 'webapp';")
