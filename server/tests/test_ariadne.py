e78930317bcc01fd40c9ed50b9d11013a16a5d88
Gaetano Guerriero <gaetano@athenian.co>
2022-06-01 14:36:10 +0200

    [DEV-4295] Utility to forbid default user in graphql

--- server/tests/test_ariadne.py
+++ server/tests/test_ariadne.py
@@ -1,3 +1,11 @@
+from dataclasses import dataclass
+
+from ariadne import gql, make_executable_schema, QueryType
+from graphql import graphql_sync
+
+from athenian.api.ariadne import ariadne_disable_default_user
+
+
 class TestGraphiQL:
     async def test(self, client) -> None:
         response = await client.request(method="GET", path="/align/ui")
@@ -5,3 +13,36 @@ class TestGraphiQL:
 
         assert "<html>" in html
         assert "graphiql.min.js" in html
+
+
+class TestAriadnDisableDefaultUser:
+
+    @dataclass
+    class FakeContext:
+        is_default_user: bool
+        uid: str = "u00"
+
+    def test(self) -> None:
+        query = QueryType()
+        type_defs = gql("""
+        type Query {
+          hello: String!
+        }
+        """)
+
+        @query.field("hello")
+        @ariadne_disable_default_user
+        def travel(_, info):
+            return "world"
+
+        schema = make_executable_schema(type_defs, [query])
+        q = "query { hello }"
+
+        result = graphql_sync(schema, q, context_value=self.FakeContext(False))
+        assert result.data == {"hello": "world"}
+        assert result.errors is None
+
+        result = graphql_sync(schema, q, context_value=self.FakeContext(True))
+        assert result.data is None
+        assert result.errors is not None
+        assert "u00 is the default user" in result.errors[0].message
