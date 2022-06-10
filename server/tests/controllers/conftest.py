343709e8a066945f154326fcfbbe66a6aecdf765
Gaetano Guerriero <gaetano@athenian.co>
2022-06-10 20:20:16 +0200

    [DEV-4350] Move some fixtures into root conftest.py

--- server/tests/controllers/conftest.py
+++ server/tests/controllers/conftest.py
@@ -19,20 +19,9 @@ from athenian.api.internal.miners.github.commit import _empty_dag, _fetch_commit
 from athenian.api.internal.miners.github.dag_accelerated import join_dags
 from athenian.api.internal.miners.github.deployment import mine_deployments
 from athenian.api.internal.miners.types import PullRequestFacts, nonemin
-from athenian.api.internal.prefixer import Prefixer
-from athenian.api.internal.settings import (
-    LogicalRepositorySettings,
-    ReleaseMatch,
-    ReleaseMatchSetting,
-    ReleaseSettings,
-    default_branch_alias,
-)
+from athenian.api.internal.settings import LogicalRepositorySettings, ReleaseMatch
 from athenian.api.models.metadata.github import Branch
-from athenian.api.models.persistentdata.models import (
-    DeployedComponent,
-    DeployedLabel,
-    DeploymentNotification,
-)
+from athenian.api.models.persistentdata.models import DeployedLabel
 from athenian.api.models.state.models import (
     AccountJiraInstallation,
     JIRAProjectSetting,
@@ -48,74 +37,6 @@ from athenian.api.typing_utils import wraps
 @pytest.fixture(scope="function")
 def no_deprecation_warnings():
     warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
-
-
-@pytest.fixture(scope="session")
-def release_match_setting_tag_or_branch():
-    return ReleaseSettings(
-        {
-            "github.com/src-d/go-git": ReleaseMatchSetting(
-                branches="master", tags=".*", events=".*", match=ReleaseMatch.tag_or_branch,
-            ),
-        },
-    )
-
-
-@pytest.fixture(scope="session")
-def release_match_setting_tag():
-    return ReleaseSettings(
-        {
-            "github.com/src-d/go-git": ReleaseMatchSetting(
-                branches="", tags=".*", events=".*", match=ReleaseMatch.tag,
-            ),
-        },
-    )
-
-
-@pytest.fixture(scope="session")
-def release_match_setting_branch():
-    return ReleaseSettings(
-        {
-            "github.com/src-d/go-git": ReleaseMatchSetting(
-                branches=default_branch_alias, tags=".*", events=".*", match=ReleaseMatch.branch,
-            ),
-        },
-    )
-
-
-@pytest.fixture(scope="session")
-def release_match_setting_event():
-    return ReleaseSettings(
-        {
-            "github.com/src-d/go-git": ReleaseMatchSetting(
-                branches="", tags=".*", events=".*", match=ReleaseMatch.event,
-            ),
-        },
-    )
-
-
-@pytest.fixture(scope="session")
-def default_branches():
-    return {"src-d/go-git": "master", "src-d/gitbase": "master", "src-d/hercules": "master"}
-
-
-_branches = None
-
-
-@pytest.fixture(scope="function")
-async def branches(mdb, branch_miner, prefixer):
-    global _branches
-    if _branches is None:
-        _branches, _ = await branch_miner.extract_branches(
-            ["src-d/go-git"], prefixer, (6366825,), mdb, None,
-        )
-    return _branches
-
-
-@pytest.fixture(scope="function")
-@with_defer
-async def prefixer(mdb):
-    return await Prefixer.load((6366825,), mdb, None)
 
 
 @pytest.fixture(scope="function")
@@ -167,20 +88,6 @@ def logical_settings_labels():
     )
 
 
-@pytest.fixture(scope="session")
-def logical_settings_full():
-    return LogicalRepositorySettings(
-        {
-            "src-d/go-git/alpha": {"title": ".*[Ff]ix"},
-            "src-d/go-git/beta": {"title": ".*[Aa]dd"},
-        },
-        {
-            "src-d/go-git/alpha": {"title": ".*(2016|2019)"},
-            "src-d/go-git/beta": {"title": "prod|.*2019"},
-        },
-    )
-
-
 @pytest.fixture(scope="function")
 async def logical_settings_db(sdb):
     await sdb.execute(
@@ -222,21 +129,6 @@ async def logical_settings_db(sdb):
                 RepositorySet.updated_at: datetime.now(timezone.utc),
             },
         ),
-    )
-
-
-@pytest.fixture(scope="session")
-def release_match_setting_tag_logical(release_match_setting_tag):
-    return ReleaseSettings(
-        {
-            **release_match_setting_tag.prefixed,
-            "github.com/src-d/go-git/alpha": ReleaseMatchSetting(
-                branches="", tags=".*", events=".*", match=ReleaseMatch.tag,
-            ),
-            "github.com/src-d/go-git/beta": ReleaseMatchSetting(
-                branches="", tags=r"v4\..*", events=".*", match=ReleaseMatch.tag,
-            ),
-        },
     )
 
 
@@ -559,52 +451,6 @@ async def precomputed_deployments(
     await _precompute_deployments(
         release_match_setting_tag_or_branch, prefixer, branches, default_branches, mdb, pdb, rdb,
     )
-
-
-@pytest.fixture(scope="function")
-async def sample_deployments(rdb):
-    await rdb.execute(delete(DeployedLabel))
-    await rdb.execute(delete(DeployedComponent))
-    await rdb.execute(delete(DeploymentNotification))
-    for year, month, day, conclusion, tag, commit in (
-        (2019, 11, 1, "SUCCESS", "v4.13.1", 2755244),
-        (2018, 12, 1, "SUCCESS", "4.8.1", 2755046),
-        (2018, 12, 2, "SUCCESS", "4.8.1", 2755046),
-        (2018, 8, 1, "SUCCESS", "4.5.0", 2755028),
-        (2016, 12, 1, "SUCCESS", "3.2.0", 2755108),
-        (2018, 1, 12, "FAILURE", "4.0.0", 2757510),
-        (2018, 1, 11, "SUCCESS", "4.0.0", 2757510),
-        (2018, 1, 10, "FAILURE", "4.0.0", 2757510),
-        (2016, 7, 6, "SUCCESS", "3.1.0", 2756224),
-    ):
-        for env in ("production", "staging", "canary"):
-            name = "%s_%d_%02d_%02d" % (env, year, month, day)
-            await rdb.execute(
-                insert(DeploymentNotification).values(
-                    dict(
-                        account_id=1,
-                        name=name,
-                        conclusion=conclusion,
-                        environment=env,
-                        started_at=datetime(year, month, day, tzinfo=timezone.utc),
-                        finished_at=datetime(year, month, day, 0, 10, tzinfo=timezone.utc),
-                        created_at=datetime.now(timezone.utc),
-                        updated_at=datetime.now(timezone.utc),
-                    ),
-                ),
-            )
-            await rdb.execute(
-                insert(DeployedComponent).values(
-                    dict(
-                        account_id=1,
-                        deployment_name=name,
-                        repository_node_id=40550,
-                        reference=tag,
-                        resolved_commit_node_id=commit,
-                        created_at=datetime.now(timezone.utc),
-                    ),
-                ),
-            )
 
 
 @pytest.fixture(scope="function")
