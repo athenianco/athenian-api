86afa6115239932b92704e37e56879f97eb32482
Gaetano Guerriero <gaetano@athenian.co>
2022-06-24 12:06:13 +0200

    [DEV-4483] Support onlyWithTargets flag in goals() gql query

--- server/athenian/api/align/queries/goals.py
+++ server/athenian/api/align/queries/goals.py
@@ -1,6 +1,6 @@
 from itertools import groupby
 from operator import itemgetter
-from typing import Any, Iterable, Mapping
+from typing import Any, Callable, Iterable, Mapping, Optional, Sequence
 
 from ariadne import ObjectType
 from graphql import GraphQLResolveInfo
@@ -30,6 +30,7 @@ async def resolve_goals(
     info: GraphQLResolveInfo,
     accountId: int,
     teamId: int,
+    onlyWithTargets: bool,
     **kwargs,
 ) -> Any:
     """Serve goals() query."""
@@ -49,29 +50,17 @@ async def resolve_goals(
     team_ids = [row[Team.id.name] for row in team_rows]
     team_goal_rows = await fetch_team_goals(accountId, team_ids, info.context.sdb)
 
-    # first iter all team goal rows, grouped by goal, to collect all parameters for the single
-    # metric request
-    team_metrics_requests = []
-    team_goal_rows_per_request = []
+    goals_to_serve = []
+
+    # iter all team goal rows, grouped by goal, to build _GoalToServe object for the goal
     for _, group_team_goal_rows_iter in groupby(team_goal_rows, itemgetter(Goal.id.name)):
-        group_team_goal_rows = list(group_team_goal_rows_iter)
-        group_goal_row = group_team_goal_rows[0]  # could be any row
-
-        metric = TEMPLATES_COLLECTION[group_goal_row[Goal.template_id.name]]["metric"]
-
-        valid_from = group_goal_row[Goal.valid_from.name]
-        expires_at = group_goal_row[Goal.expires_at.name]
-        initial_interval = goal_initial_query_interval(valid_from, expires_at)
-
-        # all teams are requested in every request
-        team_metrics_request = TeamMetricsRequest(
-            [metric], (initial_interval, (valid_from, expires_at)), team_member_map,
+        goal_team_goal_rows = list(group_team_goal_rows_iter)
+        goals_to_serve.append(
+            _GoalToServe(goal_team_goal_rows, team_tree, team_member_map, onlyWithTargets),
         )
-        team_metrics_requests.append(team_metrics_request)
-        team_goal_rows_per_request.append(group_team_goal_rows)
 
     all_metric_values = await calculate_team_metrics(
-        team_metrics_requests,
+        [g.request for g in goals_to_serve],
         account=accountId,
         meta_ids=meta_ids,
         sdb=info.context.sdb,
@@ -81,18 +70,73 @@ async def resolve_goals(
         cache=info.context.cache,
         slack=info.context.app["slack"],
     )
-    res = []
-    for i, req in enumerate(team_metrics_requests):
-        initial_metrics = all_metric_values[req.time_intervals[0]][req.metrics[0]]
-        current_metrics = all_metric_values[req.time_intervals[1]][req.metrics[0]]
-        metric_values = GoalMetricValues(initial_metrics, current_metrics)
-        team_goal_rows = team_goal_rows_per_request[i]
-        group_res = _team_tree_to_goal_tree(
-            team_tree, team_goal_rows[0], team_goal_rows, metric_values,
-        )
-        res.append(group_res)
+    return [to_serve.build_goal_tree(all_metric_values).to_dict() for to_serve in goals_to_serve]
 
-    return [r.to_dict() for r in res]
+
+class _GoalToServe:
+    """A goal served in the response."""
+
+    def __init__(
+        self,
+        team_goal_rows: Sequence[Row],
+        team_tree: TeamTree,
+        team_member_map: dict[int, list[int]],
+        only_with_targets: bool,
+    ):
+        self._team_goal_rows = team_goal_rows
+        self._request, self._goal_team_tree = self._parse_rows(
+            team_goal_rows, team_tree, team_member_map, only_with_targets,
+        )
+
+    @property
+    def request(self) -> TeamMetricsRequest:
+        return self._request
+
+    def build_goal_tree(self, metric_values) -> GoalTree:
+        initial_metrics = metric_values[self._request.time_intervals[0]][self._request.metrics[0]]
+        current_metrics = metric_values[self._request.time_intervals[1]][self._request.metrics[0]]
+        metric_values = GoalMetricValues(initial_metrics, current_metrics)
+        return _team_tree_to_goal_tree(
+            self._goal_team_tree, self._team_goal_rows[0], self._team_goal_rows, metric_values,
+        )
+
+    @classmethod
+    def _parse_rows(
+        cls,
+        team_goal_rows: Sequence[Row],
+        team_tree: TeamTree,
+        team_member_map: dict[int, list[int]],
+        only_with_targets: bool,
+    ) -> tuple[TeamMetricsRequest, TeamTree]:
+        goal_row = team_goal_rows[0]  # could be any, all rows have the joined Goal columns
+        metric = TEMPLATES_COLLECTION[goal_row[Goal.template_id.name]]["metric"]
+
+        valid_from = goal_row[Goal.valid_from.name]
+        expires_at = goal_row[Goal.expires_at.name]
+        initial_interval = goal_initial_query_interval(valid_from, expires_at)
+
+        if only_with_targets:
+            # if the flag is true
+            # 1 - prune team tree to keep only branches with assigned teams
+            keep_team = _TeamFilterFromTeamGoalRows(team_goal_rows)
+            goal_team_tree = _team_tree_prune_empty_branches(team_tree, keep_team)
+            # assigned_teams is a non empty subset of all the ids in team_tree,
+            # so the pruned tree is never empty
+            assert goal_team_tree is not None
+            # 2 - in the metrics requests only ask for teams present in the pruned tree
+            goal_tree_team_ids = goal_team_tree.flatten_team_ids()
+            requested_team_members = {
+                team_id: members
+                for team_id, members in team_member_map.items()
+                if team_id in goal_tree_team_ids
+            }
+        else:
+            goal_team_tree = team_tree
+            requested_team_members = team_member_map
+        team_metrics_request = TeamMetricsRequest(
+            [metric], (initial_interval, (valid_from, expires_at)), requested_team_members,
+        )
+        return (team_metrics_request, goal_team_tree)
 
 
 @dataclass(slots=True)
@@ -145,3 +189,30 @@ def _team_tree_to_team_goal_tree(
         for child in team_tree.children
     ]
     return TeamGoalTree(team_tree, goal_value, children)
+
+
+class _TeamFilterFromTeamGoalRows:
+    def __init__(self, rows: Iterable[Row]):
+        self._team_ids = {row[TeamGoal.team_id.name] for row in rows}
+
+    def __call__(self, team_id: int):
+        return team_id in self._team_ids
+
+
+def _team_tree_prune_empty_branches(
+    team_tree: TeamTree,
+    keep_team_fn: Callable[[int], bool],
+) -> Optional[TeamTree]:
+    """Remove unwanted teams from a TeamTree.
+
+    Empty branches are pruned.
+    """
+    kept_children = [
+        pruned_child
+        for c in team_tree.children
+        if (pruned_child := _team_tree_prune_empty_branches(c, keep_team_fn)) is not None
+    ]
+    if kept_children or keep_team_fn(team_tree.id):
+        return team_tree.with_children(kept_children)
+    else:
+        return None
