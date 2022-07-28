27e1ad0ab18e15caaa599be68d7ea556f95ea84d
Gaetano Guerriero <gaetano@athenian.co>
2022-07-28 11:39:54 +0200

    [DEV-4663] Configure alembic with compare_types=True

--- server/athenian/api/models/state/env.py
+++ server/athenian/api/models/state/env.py
@@ -44,6 +44,7 @@ def run_migrations_offline():
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        compare_type=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -65,7 +66,9 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_type=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
