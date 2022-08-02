9e7da11d0195f92c3acfc9cbf63a0f6861eb37c6
Gaetano Guerriero <gaetano@athenian.co>
2022-08-02 11:13:54 +0200

    [DEV-3862] Delete duplicated tests about internal.logical_repos

--- server/tests/controllers/test_settings_controller.py
+++ server/tests/controllers/test_settings_controller.py
@@ -8,11 +8,6 @@ from sqlalchemy import and_, delete, insert, select, update
 from athenian.api import auth
 from athenian.api.async_utils import read_sql_query
 from athenian.api.defer import wait_deferred, with_defer
-from athenian.api.internal.logical_repos import (
-    coerce_logical_repos,
-    contains_logical_repos,
-    drop_logical_repo,
-)
 from athenian.api.internal.miners.filters import JIRAFilter, LabelFilter
 from athenian.api.internal.settings import LogicalRepositorySettings, ReleaseMatch, Settings
 from athenian.api.models.metadata.github import PullRequest
@@ -1050,27 +1045,6 @@ async def test_set_work_type_empty_rules(client, headers, sdb):
         "name": "Bug Fixing",
         "rules": [],
     }
-
-
-async def test_drop_logical_repo():
-    assert drop_logical_repo("src-d/go-git") == "src-d/go-git"
-    assert drop_logical_repo("src-d/go-git/alpha") == "src-d/go-git"
-    assert drop_logical_repo("") == ""
-
-
-async def test_coerce_logical_repos():
-    assert coerce_logical_repos(["src-d/go-git"]) == {"src-d/go-git": {"src-d/go-git"}}
-    assert coerce_logical_repos(["src-d/go-git/alpha", "src-d/go-git/beta"]) == {
-        "src-d/go-git": {"src-d/go-git/alpha", "src-d/go-git/beta"},
-    }
-    assert coerce_logical_repos([]) == {}
-
-
-async def test_contains_logical_repos():
-    assert not contains_logical_repos([])
-    assert not contains_logical_repos(["src-d/go-git"])
-    assert contains_logical_repos(["src-d/go-git/"])
-    assert contains_logical_repos(["src-d/go-git/alpha"])
 
 
 @pytest.mark.parametrize("with_title", [False, True])
