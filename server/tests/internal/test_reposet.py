e76615b022fc472e07173de4f85d67f5eaaad85d
Gaetano Guerriero <gaetano@athenian.co>
2022-08-04 11:43:45 +0200

    [DEV-4721] Move test_reposet.py to right package

--- server/tests/internal/test_reposet.py
+++ server/tests/internal/test_reposet.py
@@ -14,51 +14,50 @@ from athenian.api.models.state.models import RepositorySet
 from athenian.api.response import ResponseError
 
 
-async def test_load_account_reposets_transaction(sdb, mdb_rw):
-    mdb = mdb_rw
-    await sdb.execute(delete(RepositorySet))
+class TestLoadAccountReposets:
+    async def test_transaction(self, sdb, mdb_rw):
+        mdb = mdb_rw
+        await sdb.execute(delete(RepositorySet))
 
-    async def login():
-        return "2793551"
+        async def load():
+            return await load_account_reposets(
+                1, self.login, [RepositorySet], sdb, mdb, None, None,
+            )
 
-    async def load():
-        return await load_account_reposets(1, login, [RepositorySet], sdb, mdb, None, None)
+        items = await asyncio.gather(*(load() for _ in range(10)), return_exceptions=True)
+        errors = sum(isinstance(item, ResponseError) for item in items)
+        assert errors > 0
+        items = [{**i[0]} for i in items if not isinstance(i, ResponseError)]
+        assert len(items) > 0
+        for i in items[1:]:
+            assert i == items[0]
 
-    items = await asyncio.gather(*(load() for _ in range(10)), return_exceptions=True)
-    errors = sum(isinstance(item, ResponseError) for item in items)
-    assert errors > 0
-    items = [{**i[0]} for i in items if not isinstance(i, ResponseError)]
-    assert len(items) > 0
-    for i in items[1:]:
-        assert i == items[0]
-
-
-async def test_load_account_reposets_calmness(sdb, mdb_rw):
-    await sdb.execute(delete(RepositorySet))
-    await mdb_rw.execute(
-        update(FetchProgress).values(
-            {
-                FetchProgress.updated_at: datetime.now(timezone.utc),
-            },
-        ),
-    )
-
-    async def login():
-        return "2793551"
-
-    try:
-        with pytest.raises(ResponseError):
-            await load_account_reposets(1, login, [RepositorySet], sdb, mdb_rw, None, None)
-    finally:
+    async def test_load_account_reposets_calmness(self, sdb, mdb_rw):
+        await sdb.execute(delete(RepositorySet))
         await mdb_rw.execute(
-            update(FetchProgress).values(
-                {
-                    FetchProgress.updated_at: datetime(
-                        2020, 3, 10, 14, 39, 19, tzinfo=timezone.utc,
-                    ),
-                },
+            sa.update(FetchProgress).values(
+                {FetchProgress.updated_at: datetime.now(timezone.utc)},
             ),
         )
+
+        try:
+            with pytest.raises(ResponseError):
+                await load_account_reposets(
+                    1, self.login, [RepositorySet], sdb, mdb_rw, None, None,
+                )
+        finally:
+            await mdb_rw.execute(
+                update(FetchProgress).values(
+                    {
+                        FetchProgress.updated_at: datetime(
+                            2020, 3, 10, 14, 39, 19, tzinfo=timezone.utc,
+                        ),
+                    },
+                ),
+            )
+
+    async def login(self) -> str:
+        return "2793551"
 
 
 async def test_load_account_state_no_reposet(sdb, mdb):
