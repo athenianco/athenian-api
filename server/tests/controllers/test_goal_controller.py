635e4767265dd417b46712da9f30aacb4f2479f9
Gaetano Guerriero <gaetano@athenian.co>
2022-08-10 14:28:58 +0200

    [DEV-4768] Support DB templates in get_goal_template controller

--- server/tests/controllers/test_goal_controller.py
+++ server/tests/controllers/test_goal_controller.py
@@ -1,20 +1,25 @@
 from aiohttp.test_utils import TestClient
 
 from athenian.api.align.goals.templates import TEMPLATES_COLLECTION
+from athenian.api.db import Database
 from tests.conftest import DEFAULT_HEADERS
+from tests.testutils.db import models_insert
+from tests.testutils.factory.state import GoalTemplateFactory
 
 
 class TestGetGoalTemplate:
-    async def test_from_predefined_templates(self, client: TestClient) -> None:
-        res = await self._request(client, 5)
-        assert res == {
-            "id": 5,
-            "name": TEMPLATES_COLLECTION[5]["name"],
-            "metric": TEMPLATES_COLLECTION[5]["metric"],
-        }
-
     async def test_not_found(self, client: TestClient) -> None:
-        await self._request(client, 999, 404)
+        res = await self._request(client, 999, 404)
+        assert res["type"] == "/errors/align/GoalTemplateNotFound"
+
+    async def test_base(self, client: TestClient, sdb: Database) -> None:
+        await models_insert(sdb, GoalTemplateFactory(id=200))
+
+        await self._request(client, 200)
+
+    async def test_account_mismatch(self, client: TestClient, sdb: Database) -> None:
+        await models_insert(sdb, GoalTemplateFactory(id=101, account_id=3))
+        await self._request(client, 101, 404)
 
     async def _request(self, client: TestClient, template: int, assert_status: int = 200) -> dict:
         path = f"/v1/goal_template/{template}"
