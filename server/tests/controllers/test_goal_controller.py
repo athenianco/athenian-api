688bc41fd0d2c77c2f2685ec83a2cbddfa00dbac
Gaetano Guerriero <gaetano@athenian.co>
2022-08-12 10:19:11 +0200

    [DEV-4734] Implement create_goal_template openapi operation

--- server/tests/controllers/test_goal_controller.py
+++ server/tests/controllers/test_goal_controller.py
@@ -1,9 +1,17 @@
+from typing import Any
+
 from aiohttp.test_utils import TestClient
 
 from athenian.api.db import Database
+from athenian.api.models.state.models import GoalTemplate
+from athenian.api.models.web import (
+    DeveloperMetricID,
+    GoalTemplateCreateRequest,
+    PullRequestMetricID,
+)
 from tests.conftest import DEFAULT_HEADERS
-from tests.testutils.db import models_insert
-from tests.testutils.factory.state import GoalFactory, GoalTemplateFactory
+from tests.testutils.db import assert_existing_row, assert_missing_row, models_insert
+from tests.testutils.factory.state import AccountFactory, GoalFactory, GoalTemplateFactory
 
 
 class TestGetGoalTemplate:
@@ -47,3 +55,52 @@ class TestListGoalTemplates:
         response = await client.request(method="GET", path=path, headers=DEFAULT_HEADERS)
         assert response.status == assert_status
         return await response.json()
+
+
+class BaseCreateGoalTemplateTest:
+    async def _request(self, client: TestClient, assert_status: int = 200, **kwargs: Any) -> dict:
+        path = "/v1/goal_template/create"
+        headers = DEFAULT_HEADERS
+        response = await client.request(method="POST", path=path, headers=headers, **kwargs)
+        assert response.status == assert_status
+        return await response.json()
+
+
+class TestCreateGoalTemplateErrors(BaseCreateGoalTemplateTest):
+    async def test_empty_name(self, client: TestClient, sdb: Database) -> None:
+        req = GoalTemplateCreateRequest(1, PullRequestMetricID.PR_CLOSED, "")
+        await self._request(client, 400, json=req.to_dict())
+        await assert_missing_row(sdb, GoalTemplate, account_id=1)
+
+    async def test_account_mismatch(self, client: TestClient, sdb: Database) -> None:
+        req = GoalTemplateCreateRequest(3, PullRequestMetricID.PR_CLOSED, "My template")
+        await self._request(client, 404, json=req.to_dict())
+        await assert_missing_row(sdb, GoalTemplate, account_id=3)
+
+    async def test_duplicated_name(self, client: TestClient, sdb: Database) -> None:
+        await models_insert(sdb, GoalTemplateFactory(name="T0"))
+        req = GoalTemplateCreateRequest(1, PullRequestMetricID.PR_CLOSED, "T0")
+        await self._request(client, 409, json=req.to_dict())
+
+    async def test_invalid_metric(self, client: TestClient, sdb: Database) -> None:
+        for invalid_metric in ("foo", "", DeveloperMetricID.REVIEW_REJECTIONS):
+            req = GoalTemplateCreateRequest(1, invalid_metric, "T0")
+            await self._request(client, 400, json=req.to_dict())
+        await assert_missing_row(sdb, GoalTemplate, account_id=1)
+
+
+class TestCreateGoalTemplate(BaseCreateGoalTemplateTest):
+    async def test_base(self, client: TestClient, sdb: Database) -> None:
+        req = GoalTemplateCreateRequest(1, PullRequestMetricID.PR_OPENED, "T0")
+        res = await self._request(client, json=req.to_dict())
+        template_id = res["id"]
+        await assert_existing_row(sdb, GoalTemplate, account_id=1, id=template_id, name="T0")
+
+    async def test_same_name_different_account(self, client: TestClient, sdb: Database) -> None:
+        await models_insert(
+            sdb, AccountFactory(id=11), GoalTemplateFactory(name="T0", account_id=11),
+        )
+        req = GoalTemplateCreateRequest(1, PullRequestMetricID.PR_OPENED, "T0")
+        res = await self._request(client, json=req.to_dict())
+        template_id = res["id"]
+        await assert_existing_row(sdb, GoalTemplate, account_id=1, id=template_id, name="T0")
