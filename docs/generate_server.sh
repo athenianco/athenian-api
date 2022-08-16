98c4aa4c4c9d38b723ff5fdd11c32bf7e75ea414
Gaetano Guerriero <gaetano@athenian.co>
2022-08-16 14:15:16 +0200

    [DEV-4798] Fix new generated dir permission in generate_server.sh

--- docs/generate_server.sh
+++ docs/generate_server.sh
@@ -5,5 +5,17 @@ set -eu
 projdir="$(dirname $(dirname $0))"
 cd "$projdir"
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/openapi.yaml -g python-aiohttp -o /local/server_new --git-repo-id athenianco/athenian-api --package-name athenian.api
+uid=$(id -u)
+gid=$(id -g)
+
+docker run --rm \
+       -u $uid:$gid \
+       -v ${PWD}:/local \
+       openapitools/openapi-generator-cli \
+       generate \
+       -i /local/docs/openapi.yaml \
+       -g python-aiohttp \
+       -o /local/server_new \
+       --git-repo-id athenianco/athenian-api \
+       --package-name athenian.api
 black server_new
