7f438d16b7b10f2f99e6e3cde9c23bf75f9acadf
Gaetano Guerriero <gaetano@athenian.co>
2022-08-16 14:13:21 +0200

    [DEV-4798] Make generate_server.sh PWD independent

--- docs/generate_server.sh
+++ docs/generate_server.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+set -eu
+
+projdir="$(dirname $(dirname $0))"
+cd "$projdir"
+
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/docs/openapi.yaml -g python-aiohttp -o /local/server_new --git-repo-id athenianco/athenian-api --package-name athenian.api
 black server_new
