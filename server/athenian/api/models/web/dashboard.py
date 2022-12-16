54fc63b46c8462a9617b75ee495ff4f6d9643efa
Gaetano Guerriero <gaetano@athenian.co>
2022-12-16 09:31:49 +0100

    [DEV-5521] Define DashboardChartCreateRequest web model

--- server/athenian/api/models/web/dashboard.py
+++ server/athenian/api/models/web/dashboard.py
@@ -4,16 +4,19 @@ from typing import Optional, Sequence
 from athenian.api.models.web.base_model_ import Model
 
 
-class DashboardChart(Model):
-    """A chart showing metric values in a dashboard."""
-
+class _CommonChartInfo(Model):
     date_from: Optional[date]
     date_to: Optional[date]
     description: str
-    id: int
     metric: str
     name: str
     time_interval: Optional[str]
+
+
+class DashboardChart(_CommonChartInfo):
+    """A chart showing metric values in a dashboard."""
+
+    id: int
 
 
 class TeamDashboard(Model):
@@ -22,3 +25,9 @@ class TeamDashboard(Model):
     charts: Sequence[DashboardChart]
     id: int
     team: int
+
+
+class DashboardChartCreateRequest(_CommonChartInfo):
+    """Dashboard chart creation request."""
+
+    position: Optional[int]
