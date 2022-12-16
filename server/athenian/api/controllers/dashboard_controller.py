ae15795a394577667e6dad087c1497769c73bc89
Gaetano Guerriero <gaetano@athenian.co>
2022-12-16 10:08:45 +0100

    [DEV-5521] Implement create_dashboard_chart controller

--- server/athenian/api/controllers/dashboard_controller.py
+++ server/athenian/api/controllers/dashboard_controller.py
@@ -1,14 +1,17 @@
 from aiohttp import web
 
+from athenian.api.db import Row
 from athenian.api.internal.dashboard import (
     build_dashboard_web_model,
+    create_dashboard_chart as create_dashboard_chart_in_db,
     get_dashboard as get_dashboard_from_db,
     get_dashboard_charts,
     get_team_default_dashboard,
 )
 from athenian.api.internal.team import get_team_from_db
 from athenian.api.models.state.models import TeamDashboard
-from athenian.api.request import AthenianWebRequest
+from athenian.api.models.web import CreatedIdentifier, DashboardChartCreateRequest
+from athenian.api.request import AthenianWebRequest, model_from_body
 from athenian.api.response import model_response
 
 
@@ -18,13 +21,7 @@ async def get_dashboard(
     dashboard_id: int,
 ) -> web.Response:
     """Retrieve a team dashboard."""
-    await get_team_from_db(team_id, account_id=None, user_id=request.uid, sdb_conn=request.sdb)
-    if dashboard_id == 0:
-        async with request.sdb.connection() as sdb_conn:
-            dashboard = await get_team_default_dashboard(team_id, sdb_conn)
-    else:
-        dashboard = await get_dashboard_from_db(dashboard_id, request.sdb)
-
+    dashboard = await _get_request_dashboard(request, team_id, dashboard_id)
     charts = await get_dashboard_charts(dashboard[TeamDashboard.id.name], request.sdb)
     dashboard_model = build_dashboard_web_model(dashboard, charts)
     return model_response(dashboard_model)
@@ -45,6 +42,15 @@ async def create_dashboard_chart(
     body: dict,
 ) -> web.Response:
     """Create a dashboard chart."""
+    dashboard = await _get_request_dashboard(request, team_id, dashboard_id)
+    create_request = model_from_body(DashboardChartCreateRequest, body)
+
+    async with request.sdb.connection() as sdb_conn:
+        async with sdb_conn.transaction():
+            chart_id = await create_dashboard_chart_in_db(
+                dashboard[TeamDashboard.id.name], create_request, sdb_conn,
+            )
+    return model_response(CreatedIdentifier(id=chart_id))
 
 
 async def delete_dashboard_chart(
@@ -54,3 +60,17 @@ async def delete_dashboard_chart(
     chart_id: int,
 ) -> web.Response:
     """Delete an existing dashboard chart."""
+
+
+async def _get_request_dashboard(
+    request: AthenianWebRequest,
+    team_id: int,
+    dashboard_id: int,
+) -> Row:
+    # this is to check team existence and permissions
+    await get_team_from_db(team_id, account_id=None, user_id=request.uid, sdb_conn=request.sdb)
+    if dashboard_id == 0:
+        async with request.sdb.connection() as sdb_conn:
+            return await get_team_default_dashboard(team_id, sdb_conn)
+    else:
+        return await get_dashboard_from_db(dashboard_id, request.sdb)
---
+++ server/tests/controllers/dashboard_controller/test_create_dashboard_chart.py
@@ -0,0 +1,210 @@
+from datetime import date
+from typing import Any
+
+import sqlalchemy as sa
+
+from athenian.api.db import Database, ensure_db_datetime_tz
+from athenian.api.models.state.models import DashboardChart, TeamDashboard
+from athenian.api.models.web import DashboardChartCreateRequest, PullRequestMetricID
+from tests.testutils.db import assert_existing_row, assert_missing_row, models_insert
+from tests.testutils.factory.state import DashboardChartFactory, TeamDashboardFactory, TeamFactory
+from tests.testutils.requester import Requester
+from tests.testutils.time import dt
+
+
+class BaseCreateDashboardChartTest(Requester):
+    path = "/private/team/{team_id}/dashboard/{dashboard_id}/chart/create"
+
+    async def post(self, team_id: int, dashboard_id: int, *args: Any, **kwargs: Any):
+        path_kwargs = {"team_id": team_id, "dashboard_id": dashboard_id}
+        res = await super().post(*args, path_kwargs=path_kwargs, **kwargs)
+        return res
+
+    @classmethod
+    def _body(
+        cls,
+        *,
+        description: str = "chart desc",
+        metric: str = PullRequestMetricID.PR_SIZE,
+        name: str = "chart name",
+        **kwargs: Any,
+    ) -> dict:
+        if not {"time_interval", "date_from", "date_to"}.intersection(kwargs):
+            kwargs["time_interval"] = "P3M"
+
+        req = DashboardChartCreateRequest(
+            description=description, metric=metric, name=name, **kwargs,
+        )
+        body = req.to_dict()
+        if body.get("date_from"):
+            body["date_from"] = body["date_from"].isoformat()
+        if body.get("date_to"):
+            body["date_to"] = body["date_to"].isoformat()
+        return body
+
+
+class TestCreateDashboardChartErrors(BaseCreateDashboardChartTest):
+    async def test_team_not_found(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        await self.post(20, 1, 404, json=self._body())
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_dashboard_not_found(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        await self.post(10, 2, 404, json=self._body())
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_team_account_mismatch(self, sdb: Database) -> None:
+        await models_insert(
+            sdb, TeamFactory(id=10, owner_id=3), TeamDashboardFactory(id=1, team_id=10),
+        )
+        await self.post(10, 1, 404, json=self._body())
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_invalid_time_interval(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        body = self._body(time_interval="1 year")
+        await self.post_json(10, 1, 400, json=body)
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_both_relative_and_static_interval(self, sdb: Database) -> None:
+        body = self._body(
+            time_interval="P2Y", date_from=date(2022, 1, 1), date_to=date(2022, 12, 31),
+        )
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        await self.post_json(10, 1, 400, json=body)
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_missing_intervals(self, sdb: Database) -> None:
+        body = self._body()
+        body.pop("time_interval")
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        await self.post_json(10, 1, 400, json=body)
+        await assert_missing_row(sdb, DashboardChart)
+
+    async def test_negative_position(self, sdb: Database) -> None:
+        body = self._body(position=-1)
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        await self.post_json(10, 1, 400, json=body)
+
+
+class TestCreateDashboardChart(BaseCreateDashboardChartTest):
+    async def test_first_chart(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        body = self._body(
+            name="My chart",
+            description="My chart description",
+            metric=PullRequestMetricID.PR_REVIEW_PENDING_COUNT,
+            time_interval="P1Y",
+        )
+        res = await self.post_json(10, 1, json=body)
+
+        await assert_existing_row(
+            sdb,
+            DashboardChart,
+            id=res["id"],
+            position=0,
+            name="My chart",
+            description="My chart description",
+            time_interval="P1Y",
+            time_from=None,
+            time_to=None,
+            metric=PullRequestMetricID.PR_REVIEW_PENDING_COUNT,
+            dashboard_id=1,
+        )
+
+    async def test_static_interval(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=10), TeamDashboardFactory(id=1, team_id=10))
+        body = self._body(date_from=date(2022, 4, 1), date_to=date(2022, 4, 30))
+
+        res = await self.post_json(10, 1, json=body)
+        row = await assert_existing_row(sdb, DashboardChart, id=res["id"], time_interval=None)
+
+        assert ensure_db_datetime_tz(row[DashboardChart.time_from.name], sdb) == dt(2022, 4, 1)
+        assert ensure_db_datetime_tz(row[DashboardChart.time_to.name], sdb) == dt(2022, 5, 1)
+
+    async def test_append_to_existing_charts(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            TeamFactory(id=10),
+            TeamDashboardFactory(id=1, team_id=10),
+            DashboardChartFactory(id=3, dashboard_id=1, position=10),
+            DashboardChartFactory(id=7, dashboard_id=1, position=11),
+            DashboardChartFactory(id=5, dashboard_id=1, position=12),
+        )
+        body = self._body()
+        res = await self.post_json(10, 1, json=body)
+        # new chart takes the first free position
+        await assert_existing_row(sdb, DashboardChart, id=res["id"], position=13)
+
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(10, 3), (11, 7), (12, 5), (13, res["id"])]
+
+    async def test_position_prepend(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            TeamFactory(id=9),
+            TeamDashboardFactory(id=1, team_id=9),
+            DashboardChartFactory(id=5, dashboard_id=1, position=0),
+        )
+        res = await self.post_json(9, 1, json=self._body(position=0))
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(0, res["id"]), (1, 5)]
+
+    async def test_position(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            TeamFactory(id=9),
+            TeamDashboardFactory(id=1, team_id=9),
+            DashboardChartFactory(id=5, dashboard_id=1, position=0),
+            DashboardChartFactory(id=6, dashboard_id=1, position=1),
+            DashboardChartFactory(id=7, dashboard_id=1, position=2),
+        )
+
+        r = await self.post_json(9, 1, json=self._body(position=1))
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(0, 5), (1, r["id"]), (2, 6), (3, 7)]
+
+        r2 = await self.post_json(9, 1, json=self._body(position=3))
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(0, 5), (1, r["id"]), (2, 6), (3, r2["id"]), (4, 7)]
+
+        r3 = await self.post_json(9, 1, json=self._body(position=0))
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(0, r3["id"]), (1, 5), (2, r["id"]), (3, 6), (4, r2["id"]), (5, 7)]
+
+    async def test_position_after_last(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            TeamFactory(id=9),
+            TeamDashboardFactory(id=1, team_id=9),
+            DashboardChartFactory(id=5, dashboard_id=1, position=0),
+            DashboardChartFactory(id=6, dashboard_id=1, position=1),
+        )
+        res = await self.post_json(9, 1, json=self._body(position=4))
+        rows = await sdb.fetch_all(sa.select(DashboardChart.position, DashboardChart.id))
+        assert sorted(rows) == [(0, 5), (1, 6), (2, res["id"])]
+
+    async def test_default_dashboard(self, sdb: Database) -> None:
+        await models_insert(sdb, TeamFactory(id=9))
+        res = await self.post_json(9, 0, json=self._body())
+
+        dashboard = await assert_existing_row(sdb, TeamDashboard, team_id=9)
+        await assert_existing_row(
+            sdb, DashboardChart, dashboard_id=dashboard[TeamDashboard.id.name], id=res["id"],
+        )
+
+        res2 = await self.post_json(9, 0, json=self._body())
+        await assert_existing_row(
+            sdb, DashboardChart, dashboard_id=dashboard[TeamDashboard.id.name], id=res2["id"],
+        )
+
+        res3 = await self.post_json(9, 0, json=self._body(position=1))
+        await assert_existing_row(
+            sdb, DashboardChart, dashboard_id=dashboard[TeamDashboard.id.name], id=res3["id"],
+        )
+
+        chart_id_rows = await sdb.fetch_all(
+            sa.select(DashboardChart.id).order_by(DashboardChart.position),
+        )
+        assert [r[0] for r in chart_id_rows] == [res["id"], res3["id"], res2["id"]]
