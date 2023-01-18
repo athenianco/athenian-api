b7de40beb22da44145c9050877703b07e437f033
Gaetano Guerriero <gaetano@athenian.co>
2023-01-18 09:09:03 +0100

    [DEV-5817] Forbid default user in create_token controller

--- server/tests/controllers/test_security_controller.py
+++ server/tests/controllers/test_security_controller.py
@@ -1,28 +1,52 @@
 from datetime import datetime
 import json
-import random
 
+from aiohttp import ClientResponse
 import pytest
 from sqlalchemy import insert, select
 
+from athenian.api.db import Database
 from athenian.api.models.state.models import UserToken
 from athenian.api.models.web import CreatedToken, ListedToken
-from tests.controllers.test_user_controller import vadim_email
+from tests.testutils.auth import force_request_auth
+from tests.testutils.db import models_insert
+from tests.testutils.factory.state import UserAccountFactory
 from tests.testutils.requester import Requester
+
+_USER_ID = "github|1"
 
 
 class TestCreateToken(Requester):
     path = "/v1/token/create"
 
-    @pytest.mark.flaky(reruns=5, reruns_delay=random.uniform(0.5, 2.5))
-    async def test_auth(self, client, app):
+    @pytest.fixture(scope="function", autouse=True)
+    async def _create_user(self, sdb):
+        await models_insert(sdb, UserAccountFactory(user_id=_USER_ID))
+
+    async def _request(self, *args, user_id: str | None = _USER_ID, **kwargs) -> ClientResponse:
+        with force_request_auth(user_id, self.headers) as headers:
+            return await super()._request(*args, headers=headers, **kwargs)
+
+    async def test_auth(self, client, app, sdb, eiso_user):
         body = {"account": 1, "name": "xxx"}
-        res = await self.post_json(json=body)
+        res = await self.post_json(json=body, user_id="auth0|5e1f6e2e8bfa520ea5290741")
         response = CreatedToken.from_dict(res)
         assert response.id == 1
         token = response.token
         assert token == "AQAAAAAAAAA="  # unencrypted
+
         app._auth0._default_user_id = None
+        _set_user = app._auth0._set_user
+
+        async def _new_set_user(request, token, method):
+            await _set_user(request, token, method)
+
+            async def get_user():
+                return eiso_user
+
+            request.user = get_user
+
+        app._auth0._set_user = _new_set_user
 
         headers = self.headers.copy()
         headers["X-API-Key"] = token
@@ -32,36 +56,14 @@ class TestCreateToken(Requester):
         assert response.status == 200
         res_data = await response.json()
         del res_data["updated"]
-        assert res_data == {
-            "id": "auth0|62a1ae88b6bba16c6dbc6870",
-            "email": vadim_email,
-            "name": "Vadim Markovtsev",
-            "login": "vadim",
-            "native_id": "62a1ae88b6bba16c6dbc6870",
-            "picture": "https://s.gravatar.com/avatar/d7fb46e4e35ecf7c22a1275dd5dbd303?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fva.png",  # noqa
-            "accounts": {
-                "1": {
-                    "is_admin": True,
-                    "expired": False,
-                    "has_ci": True,
-                    "has_jira": True,
-                    "has_deployments": True,
-                },
-                "2": {
-                    "is_admin": False,
-                    "expired": False,
-                    "has_ci": False,
-                    "has_jira": False,
-                    "has_deployments": False,
-                },
-            },
-        }
+        assert res_data["id"] == "auth0|5e1f6e2e8bfa520ea5290741"
 
     @pytest.mark.parametrize(
         "account, name, code",
         [(2, "yyy", 200), (3, "zzz", 404), (1, "", 400), (1, "x" * 257, 400)],
     )
-    async def test_nasty_input(self, account, name, code):
+    async def test_nasty_input(self, sdb, account, name, code):
+        await models_insert(sdb, UserAccountFactory(user_id=_USER_ID, account_id=2))
         await self.post_json(json={"account": account, "name": name}, assert_status=code)
 
     async def test_same_name(self):
@@ -69,21 +71,23 @@ class TestCreateToken(Requester):
         await self.post_json(json=body)
         await self.post_json(json=body, assert_status=409)
 
+    async def test_default_user_forbidden(self, sdb: Database) -> None:
+        body = {"account": 1, "name": "xxx"}
+        await self.post_json(assert_status=403, json=body, user_id=None)
 
-async def test_delete_token_smoke(client, headers, sdb):
-    body = {
-        "account": 1,
-        "name": "xxx",
-    }
+
+async def test_delete_token_smoke(client, headers, sdb, disable_default_user):
+    body = {"account": 1, "name": "xxx"}
     response = await client.request(
         method="POST", path="/v1/token/create", headers=headers, json=body,
     )
-    token_id = json.loads((await response.read()).decode("utf-8"))["id"]
+    assert response.status == 200
+    token_id = (await response.json())["id"]
     response = await client.request(
         method="DELETE", path="/v1/token/%d" % token_id, headers=headers, json={},
     )
     assert response.status == 200
-    assert json.loads((await response.read()).decode("utf-8")) == {}
+    assert await response.json() == {}
     assert len(await sdb.fetch_all(select([UserToken.id]))) == 0
 
 
