c38a9aed865b52ae321d0814cc69b21d43c203a5
Gaetano Guerriero <gaetano@athenian.co>
2023-01-18 08:39:50 +0100

    [DEV-5817] Refactor create_token controller tests in a single class

--- server/tests/controllers/test_security_controller.py
+++ server/tests/controllers/test_security_controller.py
@@ -8,83 +8,66 @@ from sqlalchemy import insert, select
 from athenian.api.models.state.models import UserToken
 from athenian.api.models.web import CreatedToken, ListedToken
 from tests.controllers.test_user_controller import vadim_email
+from tests.testutils.requester import Requester
 
 
-@pytest.mark.flaky(reruns=5, reruns_delay=random.uniform(0.5, 2.5))
-async def test_create_token_auth(client, headers, app):
-    body = {
-        "account": 1,
-        "name": "xxx",
-    }
-    response = await client.request(
-        method="POST", path="/v1/token/create", headers=headers, json=body,
-    )
-    response = CreatedToken.from_dict(json.loads((await response.read()).decode("utf-8")))
-    assert response.id == 1
-    token = response.token
-    assert token == "AQAAAAAAAAA="  # unencrypted
-    app._auth0._default_user_id = None
-    headers = headers.copy()
-    headers["X-API-Key"] = token
-    response = await client.request(method="GET", path="/v1/user", headers=headers, json={})
-    assert response.status == 200
-    response = json.loads((await response.read()).decode("utf-8"))
-    del response["updated"]
-    assert response == {
-        "id": "auth0|62a1ae88b6bba16c6dbc6870",
-        "email": vadim_email,
-        "name": "Vadim Markovtsev",
-        "login": "vadim",
-        "native_id": "62a1ae88b6bba16c6dbc6870",
-        "picture": "https://s.gravatar.com/avatar/d7fb46e4e35ecf7c22a1275dd5dbd303?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fva.png",  # noqa
-        "accounts": {
-            "1": {
-                "is_admin": True,
-                "expired": False,
-                "has_ci": True,
-                "has_jira": True,
-                "has_deployments": True,
+class TestCreateToken(Requester):
+    path = "/v1/token/create"
+
+    @pytest.mark.flaky(reruns=5, reruns_delay=random.uniform(0.5, 2.5))
+    async def test_auth(self, client, app):
+        body = {"account": 1, "name": "xxx"}
+        res = await self.post_json(json=body)
+        response = CreatedToken.from_dict(res)
+        assert response.id == 1
+        token = response.token
+        assert token == "AQAAAAAAAAA="  # unencrypted
+        app._auth0._default_user_id = None
+
+        headers = self.headers.copy()
+        headers["X-API-Key"] = token
+        response = await self.client.request(
+            method="GET", path="/v1/user", headers=headers, json={},
+        )
+        assert response.status == 200
+        res_data = await response.json()
+        del res_data["updated"]
+        assert res_data == {
+            "id": "auth0|62a1ae88b6bba16c6dbc6870",
+            "email": vadim_email,
+            "name": "Vadim Markovtsev",
+            "login": "vadim",
+            "native_id": "62a1ae88b6bba16c6dbc6870",
+            "picture": "https://s.gravatar.com/avatar/d7fb46e4e35ecf7c22a1275dd5dbd303?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fva.png",  # noqa
+            "accounts": {
+                "1": {
+                    "is_admin": True,
+                    "expired": False,
+                    "has_ci": True,
+                    "has_jira": True,
+                    "has_deployments": True,
+                },
+                "2": {
+                    "is_admin": False,
+                    "expired": False,
+                    "has_ci": False,
+                    "has_jira": False,
+                    "has_deployments": False,
+                },
             },
-            "2": {
-                "is_admin": False,
-                "expired": False,
-                "has_ci": False,
-                "has_jira": False,
-                "has_deployments": False,
-            },
-        },
-    }
+        }
 
-
-@pytest.mark.parametrize(
-    "account, name, code",
-    [(2, "yyy", 200), (3, "zzz", 404), (1, "", 400), (1, "x" * 257, 400)],
-)
-async def test_create_token_nasty_input(client, headers, account, name, code):
-    body = {
-        "account": account,
-        "name": name,
-    }
-    response = await client.request(
-        method="POST", path="/v1/token/create", headers=headers, json=body,
+    @pytest.mark.parametrize(
+        "account, name, code",
+        [(2, "yyy", 200), (3, "zzz", 404), (1, "", 400), (1, "x" * 257, 400)],
     )
-    body = json.loads((await response.read()).decode("utf-8"))
-    assert response.status == code, body
+    async def test_nasty_input(self, account, name, code):
+        await self.post_json(json={"account": account, "name": name}, assert_status=code)
 
-
-async def test_create_token_same_name(client, headers):
-    body = {
-        "account": 1,
-        "name": "xxx",
-    }
-    response = await client.request(
-        method="POST", path="/v1/token/create", headers=headers, json=body,
-    )
-    assert response.status == 200
-    response = await client.request(
-        method="POST", path="/v1/token/create", headers=headers, json=body,
-    )
-    assert response.status == 409
+    async def test_same_name(self):
+        body = {"account": 1, "name": "xxx"}
+        await self.post_json(json=body)
+        await self.post_json(json=body, assert_status=409)
 
 
 async def test_delete_token_smoke(client, headers, sdb):
