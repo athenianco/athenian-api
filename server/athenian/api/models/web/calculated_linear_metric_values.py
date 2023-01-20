fcba11e5a3a01b6011009def25b67e14ddb409fe
Gaetano Guerriero <gaetano@athenian.co>
2023-01-20 12:20:37 +0100

    [DEV-5733] Refactor cleaning up confidence scores after metrics calculation

--- server/athenian/api/models/web/calculated_linear_metric_values.py
+++ server/athenian/api/models/web/calculated_linear_metric_values.py
@@ -12,3 +12,12 @@ class CalculatedLinearMetricValues(Model):
     confidence_scores: Optional[list[int]]
     confidence_mins: Optional[list[object]]
     confidence_maxs: Optional[list[object]]
+
+    def clean_up_confidence_fields(self) -> None:
+        """Clear the confidence fields when `confidence_scores` contains nothing, in place."""
+        if self.confidence_scores is None or not any(
+            score is not None for score in self.confidence_scores
+        ):
+            self.confidence_mins = None
+            self.confidence_maxs = None
+            self.confidence_scores = None
