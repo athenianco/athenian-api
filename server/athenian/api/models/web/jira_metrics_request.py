257e2be13a1412ec04cca909cb159e9043d78031
Gaetano Guerriero <gaetano@athenian.co>
2023-02-07 12:31:51 +0100

    [DEV-5735] Add for to JIRAMetricsRequest web model

--- server/athenian/api/models/web/jira_metrics_request.py
+++ server/athenian/api/models/web/jira_metrics_request.py
@@ -3,6 +3,7 @@ from typing import Optional
 from athenian.api.models.web.base_model_ import AllOf, Model
 from athenian.api.models.web.common_metrics_properties import CommonMetricsProperties
 from athenian.api.models.web.filter_jira_common import FilterJIRACommon
+from athenian.api.models.web.jira_filter import JIRAFilter
 from athenian.api.models.web.jira_filter_with import JIRAFilterWith
 from athenian.api.models.web.jira_metric_id import JIRAMetricID
 
@@ -10,10 +11,16 @@ from athenian.api.models.web.jira_metric_id import JIRAMetricID
 class JIRAMetricsRequestSpecials(Model, sealed=False):
     """Request body of `/metrics/jira`."""
 
-    with_: (Optional[list[JIRAFilterWith]], "with")
+    with_: Optional[list[JIRAFilterWith]]
     metrics: list[str]
     epics: Optional[list[str]]
     group_by_jira_label: Optional[bool]
+    for_: Optional[list[JIRAFilter]]
+
+    attribute_map = {
+        "with_": "with",
+        "for_": "for",
+    }
 
     def validate_metrics(self, metrics: list[str]) -> list[str]:
         """Sets the metrics of this JIRAMetricsRequest.
