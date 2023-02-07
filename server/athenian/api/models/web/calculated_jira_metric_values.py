ce83eb5ba7dcf3a73d65445ce9dcdae7582fdec5
Gaetano Guerriero <gaetano@athenian.co>
2023-02-07 12:32:16 +0100

    [DEV-5735] Add for to CalculatedJIRAMetricValues web model

--- server/athenian/api/models/web/calculated_jira_metric_values.py
+++ server/athenian/api/models/web/calculated_jira_metric_values.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from athenian.api.models.web.base_model_ import Model
 from athenian.api.models.web.calculated_linear_metric_values import CalculatedLinearMetricValues
 from athenian.api.models.web.granularity import GranularityMixin
+from athenian.api.models.web.jira_filter import JIRAFilter
 from athenian.api.models.web.jira_filter_with import JIRAFilterWith
 
 
@@ -10,6 +11,12 @@ class CalculatedJIRAMetricValues(Model, GranularityMixin):
     """Calculated JIRA metrics for a specific granularity."""
 
     granularity: str
+    for_: Optional[JIRAFilter]
     jira_label: Optional[str]
-    with_: (Optional[JIRAFilterWith], "with")
+    with_: Optional[JIRAFilterWith]
     values: List[CalculatedLinearMetricValues]
+
+    attribute_map = {
+        "with_": "with",
+        "for_": "for",
+    }
