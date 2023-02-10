3df1a8348b3e1ed28a4d0b7646501bfe970211c8
Gaetano Guerriero <gaetano@athenian.co>
2023-02-10 11:01:53 +0100

    [DEV-5738] Support grouping by labels in calc_jira_metrics

--- server/tests/internal/features/test_metric_calculator.py
+++ server/tests/internal/features/test_metric_calculator.py
@@ -162,10 +162,10 @@ class TestGroupJIRAFactsByJIRA:
 
     def test_single_group(self) -> None:
         df = self._make_df(
-            (b"p0", b"t0", None),
-            (b"p0", b"t1", b"pr0"),
-            (b"p0", b"t0", b"pr0"),
-            (b"p1", b"t1", None),
+            (b"p0", b"t0", None, None),
+            (b"p0", b"t1", b"pr0", None),
+            (b"p0", b"t0", b"pr0", None),
+            (b"p1", b"t1", None, None),
         )
         jira_groups = [JIRAGrouping(["p0"], types=["t0"])]
         res = group_jira_facts_by_jira(jira_groups, df)
@@ -184,14 +184,14 @@ class TestGroupJIRAFactsByJIRA:
 
     def test_multiple_groups(self) -> None:
         df = self._make_df(
-            (b"p0", b"t0", None),
-            (b"p0", b"t1", b"pr0"),
-            (b"p0", b"t0", None),
-            (b"p0", b"t0", b"pr0"),
-            (b"p0", b"t1", None),
-            (b"p1", b"t2", b"pr0"),
-            (b"p1", b"t0", b"pr0"),
-            (b"p1", b"t0", b"pr1"),
+            (b"p0", b"t0", None, None),
+            (b"p0", b"t1", b"pr0", None),
+            (b"p0", b"t0", None, None),
+            (b"p0", b"t0", b"pr0", None),
+            (b"p0", b"t1", None, None),
+            (b"p1", b"t2", b"pr0", None),
+            (b"p1", b"t0", b"pr0", None),
+            (b"p1", b"t0", b"pr1", None),
         )
         jira_groups = [
             JIRAGrouping(["p1"]),
@@ -214,17 +214,32 @@ class TestGroupJIRAFactsByJIRA:
         jira_groups = [JIRAGrouping(None, ["t0", "t1"], None)]
 
         df = self._make_df(
-            (None, None, b"t0"),
-            (None, b"pr0", b"t1"),
-            (None, None, b"t2"),
-            (None, None, b"t0"),
+            (None, None, b"t0", None),
+            (None, b"pr0", b"t1", None),
+            (None, None, b"t2", None),
+            (None, None, b"t0", None),
         )
         df = df.drop(Issue.project_id.name, axis="columns")
         res = group_jira_facts_by_jira(jira_groups, df)
         assert len(res) == 1
         assert_array_equal(res[0], np.array([0, 1, 3]))
 
+    def test_labels(self) -> None:
+        jira_groups = [JIRAGrouping(labels=["l0"])]
+        df = self._make_df(
+            (None, None, None, ["l0"]),
+            (None, None, None, []),
+            (None, None, None, ["l1"]),
+            (None, None, None, ["l0", "l1"]),
+        )
+        res = group_jira_facts_by_jira(jira_groups, df)
+        assert len(res) == 1
+        assert_array_equal(res[0], np.array([0, 3]))
+
     @classmethod
     def _make_df(cls, *rows: tuple) -> pd.DataFrame:
-        columns = [Issue.project_id.name, Issue.type_id.name, Issue.priority_id.name]
-        return pd.DataFrame.from_records(rows, columns=columns).astype("S")
+        columns = [Issue.project_id, Issue.type_id, Issue.priority_id, Issue.labels]
+
+        df = pd.DataFrame.from_records(rows, columns=[c.name for c in columns])
+        types = {"project_id": "S", "type_id": "S", "priority_id": "S"}
+        return df.astype(types)
