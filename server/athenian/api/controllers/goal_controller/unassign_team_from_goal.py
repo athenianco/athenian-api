7d89ecdd585a27a2f3471c66383303095c7484ea
Gaetano Guerriero <gaetano@athenian.co>
2023-02-28 16:49:22 +0100

    [DEV-6108] Implement unassign_team_from_goal controller w/o recursive

---
+++ server/athenian/api/controllers/goal_controller/unassign_team_from_goal.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+
+from aiohttp import web
+
+from athenian.api.align.exceptions import GoalNotFoundError
+from athenian.api.align.goals.dbaccess import (
+    fetch_goal_account,
+    unassign_team_from_goal as unassign_team_from_goal_in_db,
+)
+from athenian.api.auth import disable_default_user
+from athenian.api.internal.account import request_user_belongs_to_account
+from athenian.api.models.web import CreatedIdentifier, GoalUnassignTeamRequest
+from athenian.api.request import AthenianWebRequest, model_from_body
+from athenian.api.response import model_response
+
+
+@disable_default_user
+async def unassign_team_from_goal(
+    request: AthenianWebRequest,
+    id: int,
+    body: dict,
+) -> web.Response:
+    """Unassign a team from a goal. The Goal is deleted if the goal has no more teams assigned."""
+    unassign_req = model_from_body(GoalUnassignTeamRequest, body)
+    async with request.sdb.connection() as sdb_conn:
+        async with sdb_conn.transaction():
+            account = await fetch_goal_account(id, sdb_conn)
+            if not await request_user_belongs_to_account(request, account):
+                raise GoalNotFoundError(id)
+            still_existing = await unassign_team_from_goal_in_db(
+                account, id, unassign_req.team, sdb_conn,
+            )
+
+    if still_existing:
+        return model_response(CreatedIdentifier(id=id))
+    return web.Response(status=HTTPStatus.NO_CONTENT)
---
+++ server/tests/controllers/goal_controller/test_unassign_team_from_goal.py
@@ -0,0 +1,105 @@
+from typing import Any
+
+from aiohttp import ClientResponse
+import pytest
+
+from athenian.api.db import Database
+from athenian.api.models.state.models import Goal, Team, TeamGoal
+from tests.testutils.auth import force_request_auth
+from tests.testutils.db import assert_existing_row, assert_missing_row, models_insert
+from tests.testutils.factory.state import (
+    GoalFactory,
+    TeamFactory,
+    TeamGoalFactory,
+    UserAccountFactory,
+)
+from tests.testutils.requester import Requester
+
+_USER_ID = "github|1"
+
+
+class BaseUnassignTeamFromGoalTest(Requester):
+    path = "/private/align/goal/{id}/unassign_team"
+
+    @pytest.fixture(scope="function", autouse=True)
+    async def _create_user(self, sdb):
+        await models_insert(sdb, UserAccountFactory(user_id=_USER_ID))
+
+    async def post(
+        self,
+        goal_id: int,
+        *args: Any,
+        user_id: str | None = _USER_ID,
+        **kwargs: Any,
+    ) -> ClientResponse:
+        path_kwargs = {"id": goal_id}
+        with force_request_auth(user_id, self.headers) as headers:
+            return await super().post(*args, headers=headers, path_kwargs=path_kwargs, **kwargs)
+
+
+class TestUnassignTeamFromGoalErrors(BaseUnassignTeamFromGoalTest):
+    async def test_goal_not_found(self, sdb: Database) -> None:
+        res = await self.post_json(1, json={"team": 2}, assert_status=404)
+        assert "Goal 1 not found" in res["detail"]
+
+    async def test_team_not_found(self, sdb: Database) -> None:
+        await models_insert(
+            sdb, GoalFactory(id=1), TeamFactory(id=9), TeamGoalFactory(goal_id=1, team_id=9),
+        )
+        res = await self.post_json(1, json={"team": 10}, assert_status=404)
+        assert "Team 10 not assigned" in res["detail"]
+
+    async def test_team_not_assigned(self, sdb: Database) -> None:
+        await models_insert(sdb, GoalFactory(id=1), TeamFactory(id=9))
+        res = await self.post_json(1, json={"team": 9}, assert_status=404)
+        assert "Team 9 not assigned" in res["detail"]
+
+    async def test_goal_account_mismatch(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            GoalFactory(id=1, account_id=3),
+            TeamFactory(id=9, owner_id=3),
+            TeamGoalFactory(goal_id=1, team_id=9),
+        )
+        res = await self.post_json(1, json={"team": 9}, assert_status=404)
+        assert "Goal 1 not found" in res["detail"]
+
+    async def test_default_user_forbidden(self, sdb: Database) -> None:
+        await models_insert(
+            sdb, GoalFactory(id=1), TeamFactory(id=9), TeamGoalFactory(goal_id=1, team_id=9),
+        )
+        res = await self.post_json(1, json={"team": 9}, user_id=None, assert_status=403)
+        assert "is the default user" in res["detail"]
+
+
+class TestUnassignTeamFromGoal(BaseUnassignTeamFromGoalTest):
+    async def test_non_recursive(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            GoalFactory(id=1),
+            TeamFactory(id=8),
+            TeamFactory(id=9),
+            TeamGoalFactory(goal_id=1, team_id=8),
+            TeamGoalFactory(goal_id=1, team_id=9),
+        )
+        res = await self.post_json(1, json={"team": 9})
+        assert res == {"id": 1}
+
+        await assert_existing_row(sdb, Goal, id=1)
+        await assert_missing_row(sdb, TeamGoal, team_id=9)
+        await assert_existing_row(sdb, TeamGoal, team_id=8)
+        await assert_existing_row(sdb, Team, id=8)
+        await assert_existing_row(sdb, Team, id=9)
+
+    async def test_non_recursive_last_team_unassigned(self, sdb: Database) -> None:
+        await models_insert(
+            sdb,
+            GoalFactory(id=1),
+            TeamFactory(id=8),
+            TeamGoalFactory(goal_id=1, team_id=8),
+        )
+        await self.post(1, json={"team": 8}, assert_status=204)
+
+        await assert_missing_row(sdb, Goal, id=1)
+        await assert_missing_row(sdb, TeamGoal, team_id=8)
+        await assert_existing_row(sdb, Team, id=8)
