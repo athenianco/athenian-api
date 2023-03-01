afeac426fa72f7bbae638856aa1e6ae9a3c91d85
Gaetano Guerriero <gaetano@athenian.co>
2023-03-01 10:37:36 +0100

    [DEV-5972] Avoid deprecated sa engine execute calls

--- server/athenian/api/models/state/data_52d75707d837_change_github_node_ids.py
+++ server/athenian/api/models/state/data_52d75707d837_change_github_node_ids.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 import sys
 
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 from tqdm import tqdm
 
 
@@ -9,14 +9,19 @@ def main():
     """Usage: file.py postgresql://state postgresql://metadata"""
     state_uri, metadata_uri = sys.argv[1:]
     print("Loading the accounts from sdb", file=sys.stderr)
-    state_engine = create_engine(state_uri)
-    acc_rows = state_engine.execute("SELECT id, account_id FROM account_github_accounts")
-    meta_ids = {row[1]: row[0] for row in acc_rows}
-    print("Loading the user node IDs from sdb", file=sys.stderr)
-    jira_rows = state_engine.execute(
-        "SELECT github_user_id, account_id, jira_user_id, confidence, created_at, updated_at "
-        "FROM jira_identity_mapping_old",
-    ).fetchall()
+    state_engine = sa.create_engine(state_uri)
+    with state_engine.begin() as state_conn:
+        acc_rows = state_conn.execute(
+            sa.text("SELECT id, account_id FROM account_github_accounts"),
+        )
+        meta_ids = {row[1]: row[0] for row in acc_rows}
+        print("Loading the user node IDs from sdb", file=sys.stderr)
+        jira_rows = state_conn.execute(
+            sa.text(
+                "SELECT github_user_id, account_id, jira_user_id, confidence, created_at,"
+                " updated_at FROM jira_identity_mapping_old",
+            ),
+        ).fetchall()
     node_ids = defaultdict(set)
     for row in jira_rows:
         try:
@@ -28,13 +33,16 @@ def main():
         f"in {len(node_ids)} batches",
         file=sys.stderr,
     )
-    metadata_engine = create_engine(metadata_uri)
+    metadata_engine = sa.create_engine(metadata_uri)
     mapping = defaultdict(dict)
     for acc, acc_node_ids in tqdm(node_ids.items()):
-        map_rows = metadata_engine.execute(
-            f"SELECT ext_id, node_id FROM github.graph_ids WHERE acc_id = {acc} AND ext_id = "  # noqa
-            f'ANY(VALUES {", ".join("(%r)" % n for n in acc_node_ids)})',
-        )
+        with metadata_engine.begin() as metadata_conn:
+            map_rows = metadata_conn.execute(
+                sa.text(
+                    f"SELECT ext_id, node_id FROM github.graph_ids WHERE acc_id = {acc} AND ext_id = "  # noqa
+                    f'ANY(VALUES {", ".join("(%r)" % n for n in acc_node_ids)})',
+                ),
+            )
         for row in map_rows:
             mapping[acc][row[0]] = row[1]
     print(f"Mapped {sum(len(m) for m in mapping.values())} node IDs", file=sys.stderr)
@@ -46,12 +54,13 @@ def main():
             print(f"Failed to map {row[1]}/{row[0]}")
     sql = ", ".join("(%d, %d, %r, %f, '%s', '%s')" % v for v in values)
     print(f"Inserting {len(values)} records")
-    state_engine.execute(
-        "INSERT INTO jira_identity_mapping "
-        "(github_user_id, account_id, jira_user_id, confidence, created_at, updated_at) "
-        "VALUES "
-        + sql,
-    )
+    with state_engine.begin() as state_conn:
+        state_conn.execute(
+            "INSERT INTO jira_identity_mapping "
+            "(github_user_id, account_id, jira_user_id, confidence, created_at, updated_at) "
+            "VALUES "
+            + sql,
+        )
 
 
 if __name__ == "__main__":
