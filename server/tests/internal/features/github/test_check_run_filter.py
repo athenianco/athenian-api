4444bd75bf354f5dd7ee03912bc5928211f3ba43
Gaetano Guerriero <gaetano@athenian.co>
2023-03-09 16:51:45 +0100

    [DEV-6168] Handle runs with unicode name in filter_check_runs

--- server/tests/internal/features/github/test_check_run_filter.py
+++ server/tests/internal/features/github/test_check_run_filter.py
@@ -323,6 +323,34 @@ async def test_only_queued_runs(mdb_rw: Database, sdb: Database, logical_setting
     assert items == []
 
 
+async def test_unicode_run_name(mdb_rw: Database, sdb: Database, logical_settings) -> None:
+    models = [
+        md_factory.CheckRunFactory(
+            repository_full_name="o/r",
+            repository_node_id=99,
+            started_at=dt(2022, 1, 2),
+            committed_date=dt(2022, 1, 2),
+            check_suite_status="COMPLETED",
+            name="check run 🧪",
+        ),
+    ]
+    async with DBCleaner(mdb_rw) as mdb_cleaner:
+        repo = md_factory.RepositoryFactory(node_id=99, full_name="o/r")
+        await insert_repo(repo, mdb_cleaner, mdb_rw, sdb)
+        mdb_cleaner.add_models(*models)
+        await models_insert(mdb_rw, *models)
+
+        timeline, items = await _filter(
+            time_from=dt(2022, 1, 1),
+            time_to=dt(2022, 1, 3),
+            repositories=["o/r"],
+            logical_settings=logical_settings,
+            mdb=mdb_rw,
+        )
+    assert len(items) == 1
+    assert items[0].title == "check run 🧪"
+
+
 async def _filter(**kwargs: Any) -> tuple[list[date], list[CodeCheckRunListItem]]:
     kwargs.setdefault("repositories", ["src-d/go-git"])
     kwargs.setdefault("pushers", [])
