3ca68b1039ff56f4b587e57d6a75eb1e4641c341
Gaetano Guerriero <gaetano@athenian.co>
2023-04-03 20:18:15 +0200

    [DEV-6264] Define DeploymentModifyLabelsRequest web model

--- server/athenian/api/models/web/deployment_labels.py
+++ server/athenian/api/models/web/deployment_labels.py
@@ -2,8 +2,17 @@ from typing import Any
 
 from athenian.api.models.web.base_model_ import Model
 
+_DeploymentLabels = dict[str, Any]
+
 
 class DeploymentLabelsResponse(Model):
     """Container for the labels associated with the deployments."""
 
-    labels: dict[str, Any]
+    labels: _DeploymentLabels
+
+
+class DeploymentModifyLabelsRequest(Model):
+    """Request to modify the labels associated with the deployment."""
+
+    delete: list[str] | None
+    upsert: _DeploymentLabels | None
