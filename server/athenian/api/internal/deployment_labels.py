a49ced27068cc5683320e98094f5b577c87c30d6
Gaetano Guerriero <gaetano@athenian.co>
2023-04-03 20:17:51 +0200

    [DEV-6264] Implement internal upsert_deployment_labels and delete_deployment_labels

--- server/athenian/api/internal/deployment_labels.py
+++ server/athenian/api/internal/deployment_labels.py
@@ -1,9 +1,9 @@
 from http import HTTPStatus
-from typing import Any
+from typing import Any, Sequence
 
 import sqlalchemy as sa
 
-from athenian.api.db import DatabaseLike
+from athenian.api.db import Connection, DatabaseLike, conn_in_transaction, dialect_specific_insert
 from athenian.api.models.persistentdata.models import DeployedLabel, DeploymentNotification
 from athenian.api.models.web import GenericError
 from athenian.api.response import ResponseError
@@ -35,6 +35,56 @@ async def get_deployment_labels(name: str, account: int, rdb_conn: DatabaseLike)
             labels[r[DeployedLabel.key.name]] = r[DeployedLabel.value.name]
 
     return labels
+
+
+async def lock_deployment(name: str, account: int, rdb_conn: Connection) -> None:
+    """Lock the deployment for update, raises DeploymentNotFoundError if it doesn't exist."""
+    assert await conn_in_transaction(rdb_conn)
+    where = [DeploymentNotification.name == name, DeploymentNotification.account_id == account]
+    stmt = sa.select(1).where(*where).with_for_update()
+    exists = await rdb_conn.fetch_val(stmt)
+    if exists is None:
+        raise DeploymentNotFoundError(name)
+
+
+async def upsert_deployment_labels(
+    name: str,
+    account: int,
+    upsert: dict[str, Any],
+    rdb_conn: DatabaseLike,
+) -> None:
+    """Upsert the labels for the deployment."""
+    insert_values = [
+        {
+            DeployedLabel.account_id.name: account,
+            DeployedLabel.deployment_name.name: name,
+            DeployedLabel.key.name: key,
+            DeployedLabel.value.name: value,
+        }
+        for key, value in upsert.items()
+    ]
+    insert = await dialect_specific_insert(rdb_conn)
+    stmt = insert(DeployedLabel)
+    upsert_stmt = stmt.on_conflict_do_update(
+        index_elements=DeployedLabel.__table__.primary_key.columns,
+        set_={DeployedLabel.value: stmt.excluded.value},
+    )
+    await rdb_conn.execute_many(upsert_stmt, insert_values)
+
+
+async def delete_deployment_labels(
+    name: str,
+    account: int,
+    labels: Sequence[str],
+    rdb_conn: DatabaseLike,
+) -> None:
+    """Delete the labels associated to the deployment. Unexisting labels are ignored."""
+    stmt = sa.delete(DeployedLabel).where(
+        DeployedLabel.account_id == account,
+        DeployedLabel.deployment_name == name,
+        DeployedLabel.key.in_(labels),
+    )
+    await rdb_conn.execute(stmt)
 
 
 class DeploymentNotFoundError(ResponseError):
