f8a9a8f19363198917e6fdecabd219609f2cc2fb
Gaetano Guerriero <gaetano@athenian.co>
2023-04-04 08:43:06 +0200

    [DEV-6264] Implement modify_deployment_labels controller

--- server/tests/controllers/events_controller/test_deployment_labels.py
+++ server/tests/controllers/events_controller/test_deployment_labels.py
@@ -1,13 +1,16 @@
 from typing import Any
 
 from aiohttp import ClientResponse
+import pytest
 
 from athenian.api.db import Database
-from tests.testutils.db import models_insert
+from athenian.api.models.persistentdata.models import DeployedLabel
+from tests.testutils.db import assert_existing_rows, assert_missing_row, models_insert
 from tests.testutils.factory.persistentdata import (
     DeployedLabelFactory,
     DeploymentNotificationFactory,
 )
+from tests.testutils.factory.state import UserAccountFactory, UserTokenFactory
 from tests.testutils.requester import Requester
 
 
@@ -48,3 +51,114 @@ class TestGetDeploymentLabels(BaseGetDeploymentLabelsTest):
         )
         res = await self.get_json("d", token=token)
         assert res == {"labels": {"k0": ["DEV-123"], "k1": 42}}
+
+
+class BaseModifyDeploymentLabelsTest(Requester):
+    path = "/v1/events/deployment/{name}/labels"
+    _TOKEN_ID = 1
+
+    @pytest.fixture(scope="function", autouse=True)
+    async def setup_users(self, sdb):
+        await models_insert(
+            sdb,
+            UserAccountFactory(user_id="auth0|XXX"),
+            UserTokenFactory(user_id="auth0|XXX", id=self._TOKEN_ID),
+        )
+
+    async def patch(self, name: str, *args: Any, **kwargs: Any) -> ClientResponse:
+        path_kwargs = {"name": name}
+        if "token" not in kwargs:
+            kwargs["token"] = self.encode_token(self._TOKEN_ID)
+        return await super().patch(*args, path_kwargs=path_kwargs, **kwargs)
+
+
+class TestModifyDeploymentLabelsErrors(BaseModifyDeploymentLabelsTest):
+    async def test_unauthorized(self, rdb: Database) -> None:
+        await models_insert(rdb, DeploymentNotificationFactory(name="d0"))
+        await self.patch_json("d0", token=None, json={"upsert": {"a": "b"}}, assert_status=401)
+        await assert_missing_row(rdb, DeployedLabel, deployment_name="d0")
+
+    async def test_default_user(self, rdb: Database, sdb: Database) -> None:
+        await models_insert(sdb, UserTokenFactory(user_id="auth0|62a1ae88b6bba16c6dbc6870", id=2))
+        await models_insert(rdb, DeploymentNotificationFactory(name="d"))
+        body = {"upsert": {"a": "b"}}
+        await self.patch_json("d", token=self.encode_token(2), json=body, assert_status=403)
+        await assert_missing_row(rdb, DeployedLabel, deployment_name="d")
+
+    async def test_not_found(self, rdb: Database) -> None:
+        await models_insert(rdb, DeploymentNotificationFactory(name="d0"))
+        await self.patch_json("d1", json={"upsert": {"a": "b"}}, assert_status=404)
+        await assert_missing_row(rdb, DeployedLabel, deployment_name="d0")
+
+    async def test_account_mismatch(self, rdb: Database) -> None:
+        await models_insert(rdb, DeploymentNotificationFactory(account_id=4, name="d0"))
+        await self.patch_json("d0", json={"upsert": {"a": "b"}}, assert_status=404)
+        await assert_missing_row(rdb, DeployedLabel, deployment_name="d0")
+
+    async def test_label_both_in_upsert_and_delete(self, rdb: Database) -> None:
+        await models_insert(rdb, DeploymentNotificationFactory(name="d"))
+        body = {"upsert": {"a": "b"}, "delete": ["a"]}
+        res = await self.patch_json("d", json=body, assert_status=400)
+        assert res["detail"] == 'Keys cannot appear both in "delete" and "upsert": a'
+        await assert_missing_row(rdb, DeployedLabel, deployment_name="d")
+
+
+class TestModifyDeploymentLabels(BaseModifyDeploymentLabelsTest):
+    async def test_upsert(self, rdb: Database) -> None:
+        await models_insert(
+            rdb,
+            DeploymentNotificationFactory(name="d"),
+            DeployedLabelFactory(deployment_name="d", key="k0", value="v0"),
+        )
+        res = await self.patch_json("d", json={"upsert": {"k0": True, "k1": {"foo": {}}}})
+        assert res == {"labels": {"k0": True, "k1": {"foo": {}}}}
+        await self._assert_db_labels("d", rdb, k0=True, k1={"foo": {}})
+
+    async def test_delete(self, rdb: Database) -> None:
+        await models_insert(
+            rdb,
+            DeploymentNotificationFactory(name="d"),
+            DeployedLabelFactory(deployment_name="d", key="k0", value="v0"),
+            DeployedLabelFactory(deployment_name="d", key="k1", value="v1"),
+            DeployedLabelFactory(deployment_name="d", key="k2", value="v2"),
+        )
+        res = await self.patch_json("d", json={"delete": ["k0", "k2"]})
+        assert res == {"labels": {"k1": "v1"}}
+        await self._assert_db_labels("d", rdb, k1="v1")
+
+        res = await self.patch_json("d", json={"delete": ["k1"]})
+        assert res == {"labels": {}}
+        await self._assert_db_labels("d", rdb)
+
+    async def test_delete_unknown_labels(self, rdb: Database) -> None:
+        await models_insert(
+            rdb,
+            DeploymentNotificationFactory(name="d"),
+            DeployedLabelFactory(deployment_name="d", key="k0", value="v0"),
+        )
+        res = await self.patch_json("d", json={"delete": ["k1"]})
+        assert res == {"labels": {"k0": "v0"}}
+        await self._assert_db_labels("d", rdb, k0="v0")
+
+    async def test_upsert_and_delete(self, rdb: Database) -> None:
+        await models_insert(
+            rdb,
+            DeploymentNotificationFactory(name="d"),
+            DeployedLabelFactory(deployment_name="d", key="k0", value="v0"),
+            DeployedLabelFactory(deployment_name="d", key="k1", value="v1"),
+        )
+        body = {"upsert": {"k1": ["l0"], "k2": "bar"}, "delete": ["k0"]}
+        res = await self.patch_json("d", json=body)
+        assert res == {"labels": {"k1": ["l0"], "k2": "bar"}}
+        await self._assert_db_labels("d", rdb, k1=["l0"], k2="bar")
+
+    async def _assert_db_labels(self, name: str, rdb: Database, **labels) -> None:
+        if not labels:
+            await assert_missing_row(rdb, DeployedLabel, deployment_name=name)
+            return
+
+        rows = await assert_existing_rows(rdb, DeployedLabel, deployment_name=name)
+        assert len(rows) == len(labels)
+        for r in rows:
+            value = labels.pop(r[DeployedLabel.key.name])
+            assert value == r[DeployedLabel.value.name]
