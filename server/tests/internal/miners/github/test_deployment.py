1768fbe2809c523c4efb78fbe93b04ccd1495f4f
Gaetano Guerriero <gaetano@athenian.co>
2023-04-04 22:22:46 +0200

    [DEV-6264] Move a couple of misplaced tests

--- server/tests/internal/miners/github/test_deployment.py
+++ server/tests/internal/miners/github/test_deployment.py
@@ -29,14 +29,8 @@ from athenian.api.internal.miners.github.deployment import (
     mine_deployments,
     reset_broken_deployments,
 )
-from athenian.api.internal.miners.github.release_mine import mine_releases, mine_releases_by_name
-from athenian.api.internal.miners.types import (
-    DeployedComponent as DeployedComponentStruct,
-    Deployment,
-    DeploymentConclusion,
-    DeploymentFacts,
-    ReleaseFacts,
-)
+from athenian.api.internal.miners.github.release_mine import mine_releases
+from athenian.api.internal.miners.types import DeploymentConclusion, DeploymentFacts, ReleaseFacts
 from athenian.api.internal.settings import LogicalRepositorySettings
 from athenian.api.models.metadata.github import Release
 from athenian.api.models.persistentdata.models import (
@@ -2155,90 +2149,3 @@ async def test_extract_independent_ownership_smoke(dag):
         ).tolist()
         == []
     )
-
-
-proper_deployments = {
-    "Dummy deployment": Deployment(
-        name="Dummy deployment",
-        conclusion=DeploymentConclusion.SUCCESS,
-        environment="production",
-        url=None,
-        started_at=np.datetime64(datetime(2019, 11, 1, 12, 0), "us"),
-        finished_at=np.datetime64(datetime(2019, 11, 1, 12, 15), "us"),
-        components=[
-            DeployedComponentStruct(
-                repository_full_name="src-d/go-git",
-                reference="v4.13.1",
-                sha="0d1a009cbb604db18be960db5f1525b99a55d727",
-            ),
-        ],
-        labels=None,
-    ),
-}
-
-
-@with_defer
-async def test_mine_release_by_name_deployments(
-    release_match_setting_tag_or_branch,
-    prefixer,
-    precomputed_deployments,  # noqa: F811
-    mdb,
-    pdb,
-    rdb,
-):
-    names = {"36c78b9d1b1eea682703fb1cbb0f4f3144354389", "v4.0.0"}
-    releases, _, deps = await mine_releases_by_name(
-        {"src-d/go-git": names},
-        release_match_setting_tag_or_branch,
-        LogicalRepositorySettings.empty(),
-        prefixer,
-        1,
-        (6366825,),
-        mdb,
-        pdb,
-        rdb,
-        None,
-    )
-    assert deps == proper_deployments
-    assert releases.iloc[0][ReleaseFacts.f.deployments] == ["Dummy deployment"]
-
-
-@with_defer
-async def test_mine_releases_deployments(
-    release_match_setting_tag_or_branch,
-    prefixer,
-    precomputed_deployments,  # noqa: F811
-    branches,
-    default_branches,
-    mdb,
-    pdb,
-    rdb,
-):
-    releases, _, _, deps = await mine_releases(
-        ["src-d/go-git"],
-        {},
-        branches,
-        default_branches,
-        datetime(2015, 1, 1, tzinfo=timezone.utc),
-        datetime(2020, 1, 1, tzinfo=timezone.utc),
-        LabelFilter.empty(),
-        JIRAFilter.empty(),
-        release_match_setting_tag_or_branch,
-        LogicalRepositorySettings.empty(),
-        prefixer,
-        1,
-        (6366825,),
-        mdb,
-        pdb,
-        rdb,
-        None,
-        with_avatars=False,
-        with_extended_pr_details=False,
-        with_deployments=True,
-    )
-    assert deps == proper_deployments
-    assert len(releases) == 53
-    ndeps = 0
-    for rd in releases[ReleaseFacts.f.deployments]:
-        ndeps += rd is not None and rd[0] == "Dummy deployment"
-    assert ndeps == 51
