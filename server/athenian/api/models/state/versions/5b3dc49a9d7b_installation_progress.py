09d8abda870535cb455104a48656a73cd99db396
aleksei <aleksei@athenian.co>
2023-04-11 11:01:21 -0500

    [DEV-6356] installation progress

---
+++ server/athenian/api/models/state/versions/5b3dc49a9d7b_installation_progress.py
@@ -0,0 +1,62 @@
+"""Installation progress
+
+Revision ID: 5b3dc49a9d7b
+Revises: 3557bf5f319a
+Create Date: 2023-04-04 17:13:06.137518+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "5b3dc49a9d7b"
+down_revision = "3557bf5f319a"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "installation_progress",
+        sa.Column(
+            "github_account_id",
+            sa.Integer(),
+            primary_key=True,
+        ),
+        sa.Column(
+            "account_created",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "fetch_started",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "fetch_completed",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "consistency_started",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "consistency_completed",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "precompute_started",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "precompute_completed",
+            sa.TIMESTAMP(timezone=True),
+        ),
+        sa.Column(
+            "current_status",
+            sa.Text(),
+        ),
+    )
+
+
+def downgrade():
+    op.drop_table("installation_progress")
